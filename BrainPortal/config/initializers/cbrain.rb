37184864d40bb7be6e71688a94cc088cf7ec6e75
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2008-11-26 19:28:17 +0000

    Added one constant to support for DRMAA ; added case logic for setting different constants based on the hostname.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@63 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/config/initializers/cbrain.rb
+++ BrainPortal/config/initializers/cbrain.rb
@@ -11,21 +11,52 @@
 
 # CBRAIN deployment constants
 # This file is used both by BrainPortal and Bourreau
+
+require 'socket'
+
 class CBRAIN
 
-    public
+  public
 
-    Revision_info="$Id: cbrain.rb 40 2008-11-03 20:30:49Z tsherif $"
+  Revision_info="$Id: cbrain.rb 40 2008-11-03 20:30:49Z tsherif $"
 
-    # BrainPortal constants
-    Filevault_dir            = "/Users/Tarek/Code/rails/BrainPortal2/vault"
+  # BrainPortal constants
 
-    # Bourreau constants
-    Bourreau_execution_URL   = "http://localhost:2500/"
+  # Bourreau constants
+  Bourreau_execution_URL   = "http://localhost:3050/"
 
-    # Run-time checks
-    unless File.directory?(Filevault_dir) # todo: check only for BrainPortal
-        raise "CBRAIN configuration error: file vault #{Filevault_dir} does not exist!"
-    end
+  # Configuration constants that depend on the hostname
+  case Socket.gethostname
+
+    when /huia/i            # Deployment server
+      # BrainPortal constants
+      Filevault_dir            = "/home/cbrain/CBrainPortal/FileVault"
+
+      # Bourreau constants
+      DRMAA_sharedir           = "/home/sge/gridshare"
+
+    when /montague/i        # Pierre's machine
+      # BrainPortal constants
+      Filevault_dir            = "/home/prioux/CBRAIN/trunk/FileVault"
+
+      # Bourreau constants
+      DRMAA_sharedir           = "/opt/gridengine/gridshare"
+
+    when /T/i        # Tarek's machine
+      # BrainPortal constants
+      Filevault_dir            = "/Users/Tarek/Code/rails/BrainPortal/vault"
+
+      # Bourreau constants
+      DRMAA_sharedir           = "/Users/Tarek/Code/create_this/anywhere/with/mode/4755"
+
+    else
+      raise "Configuration error: unknown hostname"
+
+  end
+
+  # Run-time checks
+  unless File.directory?(Filevault_dir) # todo: check only for BrainPortal
+      raise "CBRAIN configuration error: file vault #{Filevault_dir} does not exist!"
+  end
 
 end
