448daba2611d5ab441951c54fb34a5e4cc5eddf3
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2008-11-26 18:39:02 +0000

    Removed old BrainPortal
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@57 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/db/migrate/20081019203540_add_password.rb
+++
@@ -1,9 +0,0 @@
-class AddPassword < ActiveRecord::Migration
-  def self.up
-    add_column :users, :crypt_password, :string
-  end
-
-  def self.down
-    remove_column :users, :crypt_password
-  end
-end
