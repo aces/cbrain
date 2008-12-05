bc68381fdf659205a7522787018fe0fd03cf624d
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2008-12-05 21:51:03 +0000

    Cosmetic changes.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@84 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/app/helpers/application_helper.rb
+++ BrainPortal/app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
+
+require 'time'
+
 module ApplicationHelper
   def title(page_title)
     content_for(:title)  { ' - ' + page_title}
+  end
+
+  def to_localtime(stringtime)
+     Time.parse(stringtime).localtime.strftime("%Y-%m-%d %H:%M:%S")
   end
 end
