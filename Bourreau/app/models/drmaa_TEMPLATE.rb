dc6ba9d268202db43df7519e6d51b7335c114aa1
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-03-02 20:45:51 +0000

    Typos.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@150 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- Bourreau/app/models/drmaa_TEMPLATE.rb
+++ Bourreau/app/models/drmaa_TEMPLATE.rb
@@ -12,27 +12,27 @@
 
 # GENERAL INSTRUCTIONS:
 # 
-# There are three methods that needs to be completed, setup(),
+# There are three methods that need to be completed, setup(),
 # drmaa_commands() and save_results(). These methods have the following
 # properties:
 # 
 #   a) They will all be invoked while ruby's current directory has already
 #      been changed to a work directory that is 'grid-aware' (usually,
-#      a subdirectory shared by the nodes)
+#      a subdirectory shared by the nodes).
 # 
 #   b) They all receive in their 'params' attribute a hash table containing
-#      the key-value pairs constructed on the BrainPortal side
+#      the key-value pairs constructed on the BrainPortal side.
 # 
 #   c) Except for drmaa_commands(), they should all return true when
 #      everything is OK. A false return value in setup() or save_results()
 #      will cause the object to be stuck to the state "Failed To Setup"
 #      or "Failed To Postprocess", respectively.
 # 
-#   d) The method drmaa_commands() returns an array of shell commands.
+#   d) The method drmaa_commands() must returns an array of shell commands.
 # 
-#   e) The method setup() MUST call once the method
-#      pre_synchronize_userfile(userfile) on any userfile that it will
-#      need to access; this will have the effect of scheduling a rsync
+#   e) The method setup() MUST call the method
+#      pre_synchronize_userfile(userfile) once for each userfile that it
+#      will need to access; this will have the effect of scheduling a rsync
 #      command to synchronize the userfile's content on the BrainPortal's
 #      host with the local cache. IMPORTANT: the rsync command is run
 #      JUST BEFORE the commands returned by drmaa_commands() are executed
@@ -40,13 +40,14 @@
 #      This means that the .name() and .vaultname() methods of Userfile
 #      could return path to files that do not yet exist locally!
 # 
-#   f) The method save_results() MUST call once the method
-#      post_synchronize_userfile(userfile) on any userfile it creates or
-#      updates. This will schedule a rsync command to synchronized the
+#   f) The method save_results() MUST call the method
+#      post_synchronize_userfile(userfile) once on any userfile it creates
+#      or updates. This will schedule a rsync command to synchronized the
 #      userfile's content back to the BrainPortal's host.
 
+
 # Class declaration: You need to replace the word TEMPLATE by a name
-# for the type of processing you try to accomplish.  E.g. DrmaaCivet or
+# for the type of processing you are trying to accomplish. E.g. DrmaaCivet or
 # DrmaaMinc2jiv ; casing is important, it must work with the camelize()
 # and uncamelize() Rails functions. Also, the name of the file must be
 # adjusted accordingly.
