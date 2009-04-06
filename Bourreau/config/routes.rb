ea17f9850a128472cf6318dede957c1c591fac98
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-04-06 20:33:29 +0000

    This commit is for archiving purposes only. NOT TO BE USED.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@176 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- Bourreau/config/routes.rb
+++ Bourreau/config/routes.rb
@@ -39,6 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
   #map.resources :tasks
+  map.connect  ':drmaa_tasks/ping.:format', :controller => 'tasks', :action => 'ping'
   map.resources :drmaa_tasks,     :controller => :tasks
   map.resources :drmaa_sleepers,  :controller => :tasks
   map.resources :drmaa_snoozers,  :controller => :tasks
