a8318e4905cfe3f56eb25ee3ebef667d69b5062e
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-05-12 19:42:54 +0000

    Finished refactoring civet task.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@204 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/config/environment.rb
+++ BrainPortal/config/environment.rb
@@ -26,8 +26,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   #config.gem "RedCloth", :lib => 'redcloth'
-  config.gem "cucumber"
-  config.gem "webrat"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
