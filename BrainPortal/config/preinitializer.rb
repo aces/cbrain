f0f862d19327c5bbbaca0245901d238a1a56f274
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-06-10 23:01:34 +0000

    Deleted the "full" configuration files. Added subversion Ids to some files.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@230 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/config/preinitializer.rb
+++ BrainPortal/config/preinitializer.rb
@@ -8,13 +8,7 @@
 # $Id$
 #
 
-if File.exists? File.join(RAILS_ROOT, "config", "conditional_initializers", "configuration.rb")
-  require File.join(RAILS_ROOT, "config", "conditional_initializers", "configuration.rb")
-else
-  module Initialization_Configuration
-    Configuration = 'full'
-  end 
-end
+require File.join(RAILS_ROOT, "config", "conditional_initializers", "configuration.rb")
 
 class HostConfigInfo
   include Initialization_Configuration
