2d770c4e44043dc2ff7a6f1754d0c6bb7c459aa8
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-06-16 21:45:42 +0000

    Ensure uniquess of user <> user_preference
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@253 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/app/models/user_preference.rb
+++ BrainPortal/app/models/user_preference.rb
@@ -3,7 +3,8 @@ class UserPreference < ActiveRecord::Base
   belongs_to  :data_provider
   serialize   :other_options
   
-  validates_presence_of :user_id
+  validates_presence_of   :user_id
+  validates_uniqueness_of :user_id
   
   def update_options(options = {})
     self.other_options ||= {}
