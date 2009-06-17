7a71281c76153472ecbb869614969a8aae420e21
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-06-17 21:17:36 +0000

    Changed the add preferences migration all preferences are initialized to nil.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@261 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/db/migrate/20090616210618_add_user_preferences_to_current_users.rb
+++ BrainPortal/db/migrate/20090616210618_add_user_preferences_to_current_users.rb
@@ -2,7 +2,7 @@ class AddUserPreferencesToCurrentUsers < ActiveRecord::Migration
   def self.up
     User.all.each do |user|
       unless user.user_preference
-        UserPreference.create!(:user_id => user.id, :data_provider_id => DataProvider.first.id, :bourreau_id => CBRAIN_CLUSTERS::CBRAIN_cluster_list[0])
+        UserPreference.create!(:user_id => user.id)
       end
     end
   end
