1993a584a6126d5861dbd592ed22c85df0de2d7d
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-09-24 21:47:42 +0000

    Got ajax pagination working.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@517 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/public/javascripts/application.js
+++ BrainPortal/public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+//The following code from: http://wiki.github.com/mislav/will_paginate/ajax-pagination
+document.observe("dom:loaded", function() {
+  // the element in which we will observe all clicks and capture
+  // ones originating from pagination links
+  var container = $(document.body)
+
+  if (container) {
+    container.observe('click', function(e) {
+      var el = e.element()
+      if (el.match('.pagination a')) {
+        el.up('.pagination').replace(' Loading... <BR>')
+        new Ajax.Request(el.href, { method: 'get' })
+        e.stop()
+      }
+    })
+  }
+})
