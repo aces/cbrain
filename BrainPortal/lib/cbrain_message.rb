25e211937972fcd5fce57853ee79afee6ef80f2a
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-10-08 15:35:50 +0000

    Modified defaults for CBRAINMessage class.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@554 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/lib/cbrain_message.rb
+++ BrainPortal/lib/cbrain_message.rb
@@ -1,17 +1,17 @@
 class CBRAINMessage < Exception
   attr_accessor :redirect, :type
   
-  def initialize(message, redirect, type)
+  def initialize(message, redirect = {:action  => :index}, type = :notice)
     @redirect = redirect
     @type = type.to_sym
     super(message)
   end
 end
 
-def cb_notify(message = "Something went wrong.", redirect = {:controller  => :portal, :action  => :welcome})
+def cb_notify(message = "Something went wrong.", redirect = {:action  => :index})
   raise CBRAINMessage.new(message, redirect, :notice)
 end
 
-def cb_error(message = "Something went wrong.", redirect = {:controller  => :portal, :action  => :welcome})
+def cb_error(message = "Something went wrong.", redirect = {:action  => :index})
   raise CBRAINMessage.new(message, redirect, :error)
 end
