da8452caf7f88d81fdb2e8dc9fa3c561b5b9f051
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-10-23 21:32:25 +0000

    Fixed tree view bug and a message table update bug.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@583 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/app/controllers/messages_controller.rb
+++ BrainPortal/app/controllers/messages_controller.rb
@@ -20,39 +20,13 @@ class MessagesController < ApplicationController
   # GET /messages.xml
   def index
     @messages = current_user.messages.all(:order  => "last_sent DESC")
+    @display_messages = [] #So the upper table doesn't display on the message index page.
 
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @messages }
     end
   end
-
-  # GET /messages/1
-  # GET /messages/1.xml
-  def show
-    @message = Message.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @message }
-    end
-  end
-
-  # GET /messages/new
-  # GET /messages/new.xml
-  # def new
-  #    @message = Message.new
-  # 
-  #    respond_to do |format|
-  #      format.html # new.html.erb
-  #      format.xml  { render :xml => @message }
-  #    end
-  #  end
-
-  # GET /messages/1/edit
-  # def edit
-  #     @message = Message.find(params[:id])
-  #   end
 
   # POST /messages
   # POST /messages.xml
