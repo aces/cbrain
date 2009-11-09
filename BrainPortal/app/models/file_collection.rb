e362c7df865a2cd152f5ff1707626e8350008c97
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-11-09 20:14:10 +0000

    Fixed missing statement in set size mechanism (bad cut-and-paste?).
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@611 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/app/models/file_collection.rb
+++ BrainPortal/app/models/file_collection.rb
@@ -76,6 +76,7 @@ class FileCollection < Userfile
 
   #Calculates and sets the size attribute (active recount forced)
   def set_size!
+    local_sync = self.local_sync_status
     unless local_sync && local_sync.status == "InSync"
       return false
     end
