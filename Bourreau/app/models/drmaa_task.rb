e29e33082120811d87c52b8b24b92fac32f69263
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2010-01-04 20:11:09 +0000

    Undid extraction of common methods for DrmaaTasks.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@711 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- Bourreau/app/models/drmaa_task.rb
+++ Bourreau/app/models/drmaa_task.rb
@@ -50,9 +50,6 @@ class DrmaaTask < ActiveRecord::Base
 
   Revision_info="$Id$"
 
-  # Common methods for both the BrainPortal and Bourreau DrmaaTask objects
-  include DrmaaTaskCommon
-
   # The attribute 'params' is a serialized hash table
   # containing job-specific parameters; it's up to each
   # subclass of DrmaaTask to find/use/define its content
@@ -636,6 +633,45 @@ public
   # the method capture_job_out_err()
   def capt_stderr_b64
     @capt_stderr_b64
+  end
+
+
+
+  ##################################################################
+  # Utility Pseudo-Attributes Methods
+  ################################################################## 
+
+  # Returns the task's User
+  def user
+    @user ||= User.find(self.user_id)
+  end
+
+  # Returns a simple name for the task (without the Drmaa prefix).
+  def name
+    @name ||= self.class.to_s.gsub(/^Drmaa/,"")
+  end
+
+  def bourreau
+    @bourreau ||= Bourreau.find(self.bourreau_id)
+  end
+
+  # Returns an ID string containing both the bourreau_id +b+
+  # and the task ID +t+ in format "b/t"
+  def bid_tid
+    @bid_tid ||= "#{self.bourreau_id || '?'}/#{self.id || '?'}"
+  end
+
+  # Returns an ID string containing both the bourreau_name +b+
+  # and the task ID +t+ in format "b/t"
+  def bname_tid
+    @bname_tid ||= "#{self.bourreau.name || '?'}/#{self.id || '?'}"
+  end
+
+  # Returns an ID string containing both the bourreau_name +b+
+  # and the task ID +t+ in format "b-t" ; this is suitable to
+  # be used as part of a filename.
+  def bname_tid_dashed
+    @bname_tid_dashed ||= "#{self.bourreau.name || 'Unk'}-#{self.id || 'Unk'}"
   end
 
 end
--- BrainPortal/app/models/drmaa_task.rb
+++ BrainPortal/app/models/drmaa_task.rb
@@ -39,9 +39,6 @@ class DrmaaTask < ActiveResource::Base
 
   Revision_info="$Id$"
 
-  # Common methods for both the BrainPortal and Bourreau DrmaaTask objects
-  include DrmaaTaskCommon
-
   COMPLETED_STATUS = ["Completed"]
   RUNNING_STATUS   = ["On CPU", "Queued", "New", "Data Ready", "Setting Up"]
   FAILED_STATUS    = ["Failed To Setup", "Failed To PostProcess", "Failed To Start"]
@@ -319,6 +316,45 @@ class DrmaaTask < ActiveResource::Base
   
   def self.launch_time=(time)
     @@launch_time = time
+  end
+
+
+
+  ##################################################################
+  # Utility Pseudo-Attributes Methods
+  ################################################################## 
+
+  # Returns the task's User
+  def user
+    @user ||= User.find(self.user_id)
+  end
+
+  # Returns a simple name for the task (without the Drmaa prefix).
+  def name
+    @name ||= self.class.to_s.gsub(/^Drmaa/,"")
+  end
+
+  def bourreau
+    @bourreau ||= Bourreau.find(self.bourreau_id)
+  end
+
+  # Returns an ID string containing both the bourreau_id +b+
+  # and the task ID +t+ in format "b/t"
+  def bid_tid
+    @bid_tid ||= "#{self.bourreau_id || '?'}/#{self.id || '?'}"
+  end
+
+  # Returns an ID string containing both the bourreau_name +b+
+  # and the task ID +t+ in format "b/t"
+  def bname_tid
+    @bname_tid ||= "#{self.bourreau.name || '?'}/#{self.id || '?'}"
+  end
+
+  # Returns an ID string containing both the bourreau_name +b+
+  # and the task ID +t+ in format "b-t" ; this is suitable to
+  # be used as part of a filename.
+  def bname_tid_dashed
+    @bname_tid_dashed ||= "#{self.bourreau.name || 'Unk'}-#{self.id || 'Unk'}"
   end
 
 end
