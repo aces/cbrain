a3ee300584e003be3d35c8761c0fbeef1cf2c977
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2010-01-04 20:26:46 +0000

    Fixed method visibility problem in DrmaaTask for Bourreau.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@713 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- Bourreau/app/models/drmaa_task.rb
+++ Bourreau/app/models/drmaa_task.rb
@@ -450,6 +450,45 @@ public
 
 
   ##################################################################
+  # Utility Pseudo-Attributes Methods
+  ##################################################################
+
+  # Returns the task's User
+  def user
+    @user ||= User.find(self.user_id)
+  end
+
+  # Returns a simple name for the task (without the Drmaa prefix).
+  def name
+    @name ||= self.class.to_s.gsub(/^Drmaa/,"")
+  end
+
+  def bourreau
+    @bourreau ||= Bourreau.find(self.bourreau_id)
+  end
+
+  # Returns an ID string containing both the bourreau_id +b+
+  # and the task ID +t+ in format "b/t"
+  def bid_tid
+    @bid_tid ||= "#{self.bourreau_id || '?'}/#{self.id || '?'}"
+  end
+
+  # Returns an ID string containing both the bourreau_name +b+
+  # and the task ID +t+ in format "b/t"
+  def bname_tid
+    @bname_tid ||= "#{self.bourreau.name || '?'}/#{self.id || '?'}"
+  end
+
+  # Returns an ID string containing both the bourreau_name +b+
+  # and the task ID +t+ in format "b-t" ; this is suitable to
+  # be used as part of a filename.
+  def bname_tid_dashed
+    @bname_tid_dashed ||= "#{self.bourreau.name || 'Unk'}-#{self.id || 'Unk'}"
+  end
+
+
+
+  ##################################################################
   # Protected Methods Start Here
   ##################################################################
 
@@ -633,45 +672,6 @@ public
   # the method capture_job_out_err()
   def capt_stderr_b64
     @capt_stderr_b64
-  end
-
-
-
-  ##################################################################
-  # Utility Pseudo-Attributes Methods
-  ################################################################## 
-
-  # Returns the task's User
-  def user
-    @user ||= User.find(self.user_id)
-  end
-
-  # Returns a simple name for the task (without the Drmaa prefix).
-  def name
-    @name ||= self.class.to_s.gsub(/^Drmaa/,"")
-  end
-
-  def bourreau
-    @bourreau ||= Bourreau.find(self.bourreau_id)
-  end
-
-  # Returns an ID string containing both the bourreau_id +b+
-  # and the task ID +t+ in format "b/t"
-  def bid_tid
-    @bid_tid ||= "#{self.bourreau_id || '?'}/#{self.id || '?'}"
-  end
-
-  # Returns an ID string containing both the bourreau_name +b+
-  # and the task ID +t+ in format "b/t"
-  def bname_tid
-    @bname_tid ||= "#{self.bourreau.name || '?'}/#{self.id || '?'}"
-  end
-
-  # Returns an ID string containing both the bourreau_name +b+
-  # and the task ID +t+ in format "b-t" ; this is suitable to
-  # be used as part of a filename.
-  def bname_tid_dashed
-    @bname_tid_dashed ||= "#{self.bourreau.name || 'Unk'}-#{self.id || 'Unk'}"
   end
 
 end
