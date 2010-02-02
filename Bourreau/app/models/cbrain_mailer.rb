6fd3de63e208931624f2f9ee05f6476a287bcf8d
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2010-02-02 22:55:35 +0000

    Modified cbrain mailer.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@799 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- Bourreau/app/models/cbrain_mailer.rb
+++ Bourreau/app/models/cbrain_mailer.rb
@@ -1,1 +1,27 @@
-../../../BrainPortal/app/models/cbrain_mailer.rb
+#
+# CBRAIN Project
+#
+# CBRAIN mailer
+#
+# Original author: Tarek Sherif
+#
+# $Id$
+#
+
+#Dummy version of the cbrain mailer for Bourreau.
+#Won't send e-mails, but the class is needed for 
+#Message.
+class CbrainMailer < ActionMailer::Base
+  
+  Revision_info="$Id$"
+  
+  #Send an e-mail notification of a CBRAIN message.
+  #Meant to be used by Message.send_message.
+  def message(users, content = {})
+    subject    "Dummy"
+    recipients users.map(&:email)
+    from       "no_reply@cbrain.mcgill.ca"
+    body       ""
+  end
+
+end
