6f62666f19ae492fec136f06c34e6e9050b89bfb
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2010-02-03 01:12:54 +0000

    Fixed migration-checking script.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@802 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/config/initializers/check_for_migrations.rb
+++ BrainPortal/config/initializers/check_for_migrations.rb
@@ -1,15 +1,29 @@
-puts "C> Checking for pending migrations"
+
+#
+# CBRAIN Project
+#
+# Migration Checking Initializer
+#
+# Original author: Nicolas Kassis
+#
+# $Id$
+#
+
+#-----------------------------------------------------------------------------
+puts "C> Checking for pending migrations..."
+#-----------------------------------------------------------------------------
 
 unless ARGV[0] == "db:migrate"
   if defined? ActiveRecord
     pending_migrations = ActiveRecord::Migrator.new(:up, 'db/migrate').pending_migrations
     if pending_migrations.any?
-      puts "You have #{pending_migrations.size} pending migrations:"
+      puts "C> \t- You have #{pending_migrations.size} pending migrations:"
       pending_migrations.each do |pending_migration|
-        puts '  %4d %s' % [pending_migration.version, pending_migration.name]
+        puts "C> \t\t- %4d %s" % [pending_migration.version, pending_migration.name]
       end
-      raise %{Run "rake db:migrate" to update your database then try again.}
+      puts "C> \t- Please run \"rake db:migrate\" to update your database then try again."
+      Kernel.exit
     end
-    
   end
 end
+
