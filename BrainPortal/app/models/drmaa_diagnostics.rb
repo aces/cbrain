aa5aa554c1aac786b44fbb70cf6114c94be884bb
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2010-02-16 19:49:43 +0000

    Diagnostic tasks can be launched in large batches for stress-testing.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@875 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/app/models/drmaa_diagnostics.rb
+++ BrainPortal/app/models/drmaa_diagnostics.rb
@@ -21,23 +21,33 @@ class DrmaaDiagnostics < DrmaaTask
 
   def self.get_default_args(params = {}, saved_args = {})
     params[:delay_seconds] = 0
+    params[:num_copies]    = 1
     params
   end
 
   #See DrmaaTask.
   def self.launch(params) 
     
-    file_ids   = params[:file_ids] || []
+    user_id    = params[:user_id]    || 0
+    file_ids   = params[:file_ids]   || []
+    num_copies = (params[:num_copies] || 1).to_i
+
+    user       = User.find(user_id)
     numfiles   = file_ids.size
-    files_hash = file_ids.index_by { |id| id }
+    files_hash = file_ids.index_by { |id| "F#{id}" }
+    num_copies = 100 if num_copies > 100
 
-    task             = DrmaaDiagnostics.new
-    task.description = "Diagnostics with #{numfiles} files"
-    task.params      = { :files_hash => files_hash, :delay_seconds => params[:delay_seconds] }
-    task.user_id     = params[:user_id]
-    task.save
+    CBRAIN.spawn_with_active_records_if(num_copies > 3, user, "Diagnostics Launcher") do
+      num_copies.times do |i|
+        task             = DrmaaDiagnostics.new
+        task.description = "Diagnostics with #{numfiles} files" + (num_copies > 1 ? ", copy #{i+1}." : ".")
+        task.params      = { :files_hash => files_hash, :delay_seconds => params[:delay_seconds], :copy_number => i }
+        task.user_id     = user_id
+        task.save
+      end
+    end
 
-    "Launched Diagnostics task with #{numfiles} files."
+    "Launched #{num_copies} Diagnostics tasks with #{numfiles} files each."
   end
 
 end
