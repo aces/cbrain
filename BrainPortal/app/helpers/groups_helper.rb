8fe422aca2dc9052790ba9f12ac16847e59cf056
angela <angela@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2010-07-29 18:54:10 +0000

    Created helper to change names to project instead of groups.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@1321 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/app/helpers/groups_helper.rb
+++ BrainPortal/app/helpers/groups_helper.rb
@@ -12,4 +12,16 @@ module GroupsHelper  #:nodoc:
     end
   end
 
+  def change_to_project_name(name)
+    if name == "WorkGroup"
+      "Work Project"
+    elsif name == "SiteGroup"
+      "Site Project"
+    elsif name == "UserGroup"
+      "User Project"
+    else
+      "System Project"
+    end
+  end
+  
 end
