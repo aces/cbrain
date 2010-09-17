5e84c0db4443613918c925940b7325e3381706a9
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2010-09-17 21:06:59 +0000

    Fixed bug in caching current remote resource.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@1506 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/app/models/remote_resource.rb
+++ BrainPortal/app/models/remote_resource.rb
@@ -72,7 +72,7 @@ class RemoteResource < ActiveRecord::Base
   # Returns the RemoteResource object representing
   # the current Rails application.
   def self.current_resource
-    @current_resource ||= self.find(CBRAIN::SelfRemoteResourceId) # CLASS INSTANCE VAR!
+    self.find(CBRAIN::SelfRemoteResourceId) # not cached; multiple instances of mongrel!
   end
 
   # Returns a copy of the Rails DB configuration hash currently
