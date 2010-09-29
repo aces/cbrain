c9caaf7668d5cfa974bc068733b806ca3b63b591
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2010-09-29 21:47:59 +0000

    Whoops! Forgot to actually add the userfile subtypes.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@1546 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

---
+++ BrainPortal/app/models/userfiles/image_file.rb
@@ -0,0 +1,19 @@
+#
+# CBRAIN Project
+#
+# ImageFile model
+#
+# Original author: Tarek Sherif
+#
+# $Id$
+#
+
+class ImageFile < SingleFile
+
+  Revision_info="$Id$"
+  
+  def pretty_type
+    "(Image)"
+  end
+  
+end
---
+++ BrainPortal/app/models/userfiles/text_file.rb
@@ -0,0 +1,19 @@
+#
+# CBRAIN Project
+#
+# TextFile model
+#
+# Original author: Tarek Sherif
+#
+# $Id$
+#
+
+class TextFile < SingleFile
+
+  Revision_info="$Id$"
+  
+  def pretty_type
+    "(Text)"
+  end
+  
+end
