74f6d1717ec45147565f14d35b2f1abd32578bf5
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2011-02-07 21:17:21 +0000

    Debug timer class.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@1794 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

---
+++ Bourreau/lib/debug_timer.rb
@@ -0,0 +1,1 @@
+../../BrainPortal/lib/debug_timer.rb
---
+++ BrainPortal/lib/debug_timer.rb
@@ -0,0 +1,58 @@
+# CBRAIN Project
+#
+# Original author: Tarek Sherif
+#
+# $Id$
+#
+
+class DebugTimer
+  
+  Revision_info="$Id$"
+  
+  def self.start(message = "Timer starting at: #{Time.now}")
+    puts message
+    puts self.timer.reset
+  end
+  
+  def self.reset
+    self.timer.reset
+  end
+  
+  def self.timed_puts(*args)
+    self.timer.timed_puts(*args)
+  end
+  
+  def initialize
+    @base_time      = Time.now
+    @last_timepoint = @base_time
+  end
+  
+  def reset
+    @base_time      = Time.now
+    @last_timepoint = @base_time
+  end 
+  
+  def timed_puts(message, colour)
+    method = "puts"
+    if colour
+      method = "puts_#{colour}"
+    end
+    send method, prepare_string(message)
+  end
+  
+  def prepare_string(message)
+    current_time = Time.now
+    cumul_time = current_time - @base_time
+    dif_time = current_time - @last_timepoint
+    message += ": diff=#{dif_time}s / cumul=#{cumul_time}s"
+    
+    @last_timepoint = current_time
+    message
+  end
+  
+  private
+  
+  def self.timer
+    @@timer ||= self.new
+  end
+end
