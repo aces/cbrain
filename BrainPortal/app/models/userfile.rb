d98f9953c4c5e8a21d320134186a4c425f193a1f
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2011-04-11 17:03:33 +0000

    Has no child and Has no parent filters added.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@1899 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/app/models/userfile.rb
+++ BrainPortal/app/models/userfile.rb
@@ -76,11 +76,16 @@ class Userfile < ActiveRecord::Base
   
   named_scope             :name_like, lambda { |n| {:conditions => ["userfiles.name LIKE ?", "%#{n}%"]} }
   named_scope             :file_format, lambda { |f|
-                                          format_filter = f
+                                          format_filter = Userfile.send(:subclasses).map(&:to_s).find{ |c| c == f }
                                           format_ids = Userfile.connection.select_values("select format_source_id from userfiles where format_source_id IS NOT NULL AND type='#{format_filter}'").join(",")
                                           format_ids = " OR userfiles.id IN (#{format_ids})" unless format_ids.blank?
                                           {:conditions  => "userfiles.type='#{format_filter}'#{format_ids}"}
                                         }
+  named_scope             :has_no_parent, :conditions => {:parent_id => nil}
+  named_scope             :has_no_child,  lambda {
+                                            all_parents = Userfile.connection.select_values("SELECT DISTINCT parent_id FROM userfiles WHERE parent_id IS NOT NULL").join(",")
+                                            { :conditions => "userfiles.id NOT IN (#{all_parents})" }
+                                          }
 
   class Viewer
     attr_reader :name, :partial
