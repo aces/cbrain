6fa9d87e670d868a05b125bcfcb632c6a1c9043f
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2011-05-04 20:44:46 +0000

    Updated doc for CbrainTask to correspond to new plugin mechanism. Updated task generator.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@1990 bcc1f7e1-216c-4ce3-888e-c5a8bb225047

--- BrainPortal/lib/generators/cluster_task/cluster_task_generator.rb
+++ BrainPortal/lib/generators/cluster_task/cluster_task_generator.rb
@@ -4,19 +4,20 @@ class ClusterTaskGenerator < Rails::Generator::NamedBase
 
   def manifest        
     task_manifest = record do |m|
-      m.directory "app/models/cbrain_task/#{file_name}"
-      m.directory "app/models/cbrain_task/#{file_name}/portal"
-      m.directory "app/models/cbrain_task/#{file_name}/bourreau"
-      m.template "portal_task_model.rb",  "app/models/cbrain_task/#{file_name}/portal/#{file_name}.rb"
-      m.template "cluster_task_model.rb", "app/models/cbrain_task/#{file_name}/bourreau/#{file_name}.rb"
-      m.template "partial.html.erb",      "app/views/tasks/cbrain_task/_#{file_name}.html.erb"
-      m.template "show_params.html.erb",  "app/views/tasks/show_params/_#{file_name}.html.erb"
+      m.directory "cbrain_plugins/cbrain_task/#{file_name}"
+      m.directory "cbrain_plugins/cbrain_task/#{file_name}/portal"
+      m.directory "cbrain_plugins/cbrain_task/#{file_name}/bourreau"
+      m.directory "cbrain_plugins/cbrain_task/#{file_name}/views"
+      m.template "portal_task_model.rb",  "cbrain_plugins/cbrain_task/#{file_name}/portal/#{file_name}.rb"
+      m.template "cluster_task_model.rb", "cbrain_plugins/cbrain_task/#{file_name}/bourreau/#{file_name}.rb"
+      m.template "partial.html.erb",      "cbrain_plugins/cbrain_task/#{file_name}/views/_task_params.html.erb"
+      m.template "show_params.html.erb",  "cbrain_plugins/cbrain_task/#{file_name}/views/_show_params.html.erb"
       m.template "task_options.html",     "public/doc/tasks/#{file_name}_options.html"
-     if File.exists?("app/models/cbrain_task/#{file_name}.rb")
-       logger.exists "app/models/cbrain_task/#{file_name}.rb"
+     if File.exists?("cbrain_plugins/cbrain_task/#{file_name}.rb")
+       logger.exists "cbrain_plugins/cbrain_task/#{file_name}.rb"
      else
-       logger.create "app/models/cbrain_task/#{file_name}.rb"
-       File.symlink  "cbrain_task_class_loader.rb", "app/models/cbrain_task/#{file_name}.rb"
+       logger.create "cbrain_plugins/cbrain_task/#{file_name}.rb"
+       File.symlink  "cbrain_task_class_loader.rb", "cbrain_plugins/cbrain_task/#{file_name}.rb"
      end
     end
     task_manifest
