7f31a9c97c3b353b3d54f134a7cdccc08f68e1d9
Natacha Beck <natacha.beck@mcgill.ca>
2011-08-26 14:16:05 -0400

    Added ability to lock a user account.

--- BrainPortal/db/schema.rb
+++ BrainPortal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110815204016) do
+ActiveRecord::Schema.define(:version => 20110825190526) do
 
   create_table "active_record_logs", :force => true do |t|
     t.integer  "ar_id"
@@ -291,7 +291,7 @@ ActiveRecord::Schema.define(:version => 20110815204016) do
 
   create_table "userfiles", :force => true do |t|
     t.string   "name"
-    t.decimal  "size",             :limit => 24, :precision => 24, :scale => 0
+    t.decimal  "size",             :precision => 24, :scale => 0
     t.integer  "user_id"
     t.integer  "parent_id"
     t.datetime "created_at"
@@ -329,6 +329,7 @@ ActiveRecord::Schema.define(:version => 20110815204016) do
     t.string   "city"
     t.string   "country"
     t.datetime "last_connected_at"
+    t.boolean  "account_locked"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login"
