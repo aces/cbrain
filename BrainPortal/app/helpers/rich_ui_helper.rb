400d79356da3c9a0c80194b72102f151bd52bcfa
Pierre Rioux <pierre.rioux@mcgill.ca>
2012-02-02 17:47:06 -0500

    Better tootips for descriptions.

--- BrainPortal/app/helpers/rich_ui_helper.rb
+++ BrainPortal/app/helpers/rich_ui_helper.rb
@@ -39,7 +39,8 @@ module RichUiHelper
 
     link = h(cropped_header) + " " + 
       html_tool_tip(link_to("(more)", "#")) do
-        ("<h4>#{h(header)}</h4>\n<pre>" + h(body) + "</pre>").html_safe
+        pre_body = body.blank? ? "" : "\n<pre>" + h(body) + "</pre>"
+        ("<h4>#{h(header)}</h4>#{pre_body}").html_safe
       end
     link.html_safe
   end
