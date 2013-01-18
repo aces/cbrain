4818f55208f1b054a3b5b584366e3ddfc3b378f1
Tarek Sherif <tsherif@gmail.com>
2013-01-18 13:02:55 -0500

    Will paginate array no longer needed.

--- BrainPortal/app/controllers/data_providers_controller.rb
+++ BrainPortal/app/controllers/data_providers_controller.rb
@@ -20,8 +20,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.  
 #
 
-require 'will_paginate/array'
-
 #Restful controller for the DataProvider resource.
 class DataProvidersController < ApplicationController
 
