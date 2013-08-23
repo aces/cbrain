4894c2cdb636c74345b6672a3a22d86ba1877aa5
Tristan Glatard <tristan.glatard@mcgill.ca>
2013-08-23 11:30:41 -0400

    #{4785}

--- BrainPortal/app/models/portal_task.rb
+++ BrainPortal/app/models/portal_task.rb
@@ -633,7 +633,7 @@ class PortalTask < CbrainTask
   # STDOUT, STDERR and job script.
   def capture_job_out_err(run_number=nil,stdout_lim=2000,stderr_lim=2000)
     cb_error "Cannot get task's stdout and stderr: this task is archived." if self.workdir_archived?
-    bourreau             = self.bourreau
+    bourreau             = !self.job_template_goes_to_vm? ? self.bourreau : Bourreau.where(:id => self.params[:physical_bourreau]).first 
     control              = bourreau.send_command_get_task_outputs(self.id,run_number,stdout_lim,stderr_lim)
     self.cluster_stdout = control.cluster_stdout
     self.cluster_stderr = control.cluster_stderr
