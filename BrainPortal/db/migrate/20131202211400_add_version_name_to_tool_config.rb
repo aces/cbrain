b5e2977f227f833c78b5ac2c28ab2bdb1f0b635a
Natacha Beck <natacha.beck@mcgill.ca>
2013-12-03 12:57:07 -0500

    Fixed igration for version_name in ToolConfig.

--- BrainPortal/db/migrate/20131202211400_add_version_name_to_tool_config.rb
+++ BrainPortal/db/migrate/20131202211400_add_version_name_to_tool_config.rb
@@ -1,19 +1,21 @@
 class AddVersionNameToToolConfig < ActiveRecord::Migration
   def self.up
-	add_column    :tool_configs, :version_name, :string, :after => :id
-	ToolConfig.all.each do |tc|
-	  begin
-	    tc.version_name = $1 if tc.short_description =~ /(\d+\.[\d\.]+)/
-	    tc.save!
-	  rescue ActiveRecord::RecordInvalid => e
-	  	if tc.version_name.blank?
+    add_column    :tool_configs, :version_name, :string, :after => :id
+    ToolConfig.all.each do |tc|
+      begin
+        if tc.applies_to_bourreau_and_tool?
+          tc.version_name = $1 if tc.short_description =~ /(\d+\.[\d\.]+)/
+          tc.save!
+        end
+      rescue ActiveRecord::RecordInvalid => e
+        if tc.version_name.blank?
           tc.version_name = "TC_#{tc.id}"
         else
           tc.version_name = tc.version_name + "_TC_#{tc.id}"
         end
-	    tc.save!
-	  end
-	end
+        tc.save!
+      end
+    end
   end
 
   def self.down
