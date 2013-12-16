ee31f10444a81095ec83372074319891d3a5b72d
Tristan Glatard <tristan.glatard@mcgill.ca>
2013-12-16 15:58:44 -0500

    #4912

---
+++ BrainPortal/cbrain_plugins/cbrain_task/start_vm/common/start_vm.rb
@@ -0,0 +1,54 @@
+#
+# CBRAIN Project
+#
+# Copyright (C) 2008-2012
+# The Royal Institution for the Advancement of Learning
+# McGill University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+#
+
+class CbrainTask::StartVM
+  # will raise an exception in case parameters are not valid
+  def validate_params
+    message = ""
+    message += "Missing disk image file! "  if params[:disk_image].blank?
+
+    message += "Missing VM user! "  if params[:vm_user].blank?
+
+    message += "Missing VM boot timeout! "  if params[:vm_boot_timeout].blank?
+    message += "Boot timeout has to be an integer! " if !params[:vm_boot_timeout].is_an_integer?
+
+    message += "Missing number of instances! " if params[:number_of_vms].blank? 
+    message += "Please don't try to start more than 20 instances at once for now. " if params[:number_of_vms].to_i > 20
+    message += "Number of instances has to be an integer! " if !params[:number_of_vms].is_an_integer?
+
+    message += "Missing number of job slots! " if params[:job_slots].blank? 
+    message += "Number of job slots has to be an integer! " if !params[:job_slots].is_an_integer?
+
+    bourreau = Bourreau.find(ToolConfig.find(self.tool_config_id).bourreau_id)
+    if bourreau.cms_class != "ScirOpenStack" 
+      message+= "Missing number of CPUs ! " if params[:vm_cpus].blank?
+      message+= "Number of CPUs has to be an integer! " if !params[:vm_cpus].is_an_integer?
+
+      message+= "Missing RAM! " if params[:vm_ram_gb].blank? 
+      message+= "RAM has to be a numeric value " if !params[:vm_ram_gb].is_a_float?
+    else
+      message+= "Missing OpenStack flavor " if params[:open_stack_image_flavor].blank? 
+    end
+
+    raise message unless message == ""
+
+  end
+end
