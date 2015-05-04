b1dd4751b993e64724799830767045c4db408f08
Remi Bernard <remi.bernard@mail.mcgill.ca>
2015-05-04 14:41:26 -0400

    Re-ordering of Previous File/Next File links
    The links now follow the order and filtering parameters set by the user
    when visiting the file index. The previous/next links to files of the
    same type has been removed, as that functionality is now convered by
    filtering the index by type and then using the regular previous/next
    links. Also, as to make the functionality more intuitive, the links now
    bear the name of the file they point to.

--- BrainPortal/app/helpers/userfiles_helper.rb
+++ BrainPortal/app/helpers/userfiles_helper.rb
@@ -68,69 +68,27 @@ module UserfilesHelper
     html.join.html_safe
   end
 
-  def shift_file_link(userfile, dir, same_type, options = {})
-    if dir.to_s.downcase == "previous"
-      direction = "previous"
+  def neighbor_file_link(neighbor, index, dir, options = {})
+    return "" unless neighbor
+
+    if dir == :previous
+      text   = "<< " + neighbor.name
     else
-      direction = "next"
+      text   = neighbor.name + " >>"
     end
 
+    action       = params[:action] #Should be show or edit.
     link_options = options.delete(:html)
-    options[:conditions] ||= {}
 
-    if current_project && !(options[:conditions].has_key?(:group_id) || options[:conditions].has_key?("userfiles.group_id"))
-      options[:conditions]["userfiles.group_id"] = current_project.id
-    end
-
-    if same_type
-      options[:conditions]["userfiles.type"] = userfile.class.name
-      text = "#{direction.capitalize} #{userfile.class.name}"
-    else
-      text = "#{direction.capitalize} File"
-    end
-
-    if direction == "previous"
-      text = "<< " + text
-    else
-      text += " >>"
-    end
-
-    file = userfile.send("#{direction}_available_file", current_user, options, :name)
-
-    action = params[:action] #Should be show or edit.
-
-
-    if file
-      link_to text, {:action  => action, :id  => file.id}, link_options
-    else
-      ""
-    end
+    link_to text, {:action  => action, :id  => neighbor.id, :sort_index => index}, link_options
   end
 
-  def next_file_link(userfile, options = {})
-    shift_file_link(userfile, :next, false, options)
-  end
-
-  def previous_file_link(userfile, options = {})
-    shift_file_link(userfile, :previous, false, options)
-  end
-
-  def next_typed_file_link(userfile, options = {})
-    shift_file_link(userfile, :next, true, options)
-  end
-
-  def previous_typed_file_link(userfile, options = {})
-    shift_file_link(userfile, :previous, true, options)
-  end
-
-  def file_link_table(userfile, options = {})
+  def file_link_table(previous_userfile, next_userfile, sort_index, options = {})
     (
     "<div class=\"display_table\" style=\"width:100%\">" +
       "<div class=\"display_row\">" +
-        "<div class=\"display_cell\">#{previous_file_link(@userfile, options.clone)}</div><div class=\"display_cell\" style=\"text-align:right\">#{next_file_link(@userfile, options.clone)}</div>" +
-      "</div>" +
-      "<div class=\"display_row\">" +
-        "<div class=\"display_cell\">#{previous_typed_file_link(@userfile, options.clone)}</div><div class=\"display_cell\" style=\"text-align:right\">#{next_typed_file_link(@userfile, options.clone)}</div>" +
+        "<div class=\"display_cell\">#{neighbor_file_link(previous_userfile, [0, sort_index - 1].max, :previous, options.clone)}</div>" +
+        "<div class=\"display_cell\" style=\"text-align:right\">#{neighbor_file_link(next_userfile, sort_index + 1, :next, options.clone)}</div>" +
       "</div>" +
     "</div>"
     ).html_safe
