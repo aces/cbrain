40b15aafd0587be0ef1a34694c8b330441e4ddd4
Remi Bernard <remi.bernard@mail.mcgill.ca>
2015-07-03 18:22:35 -0400

    Delegated event bindings for dynamic-table.js & JS partials
    All events are now bound (via delegate) to the main dynamic_table
    element (.dynamic_table container div) instead of their direct targets.
    Switching from direct bindings to delegated ones yields a nice
    performance boost aswell as making row insertion and removal easier (no
    need to do a full rebind).
    
    This changeset also fixes a few minor UI quirks:
    - Fix for column visibility icons incorrectly being white instead of black
    - Fixed pagination width for the tasks index
    - Clicking on a batch's type now expands/collapses/opens the batch
      instead of selecting it.

--- BrainPortal/public/javascripts/dynamic-table.js
+++ BrainPortal/public/javascripts/dynamic-table.js
@@ -37,31 +37,17 @@
     };
   }
 
-  /*
-   * reload event
-   * Present on .dynamic-table nodes, refreshes the dynamic table's state and
-   * event bindings. Trigger this event if the table has changed (rows have
-   * been added/removed, for example) and the current state is no longer valid.
-   */
-  $(document).delegate('.dynamic-table', 'reload.dyn-tbl', function () {
+  /* jQuery UI icons */
+  var icons = {
+    column_show: "ui-icon-radio-off",
+    column_hide: "ui-icon-radio-on"
+  };
 
-    var container      = $(this),
-        dyntbl_id      = container.attr('id'),
-        table          = container.find('.dt-table'),
-        request_type   = container.data('request-type'),
-        selection_mode = container.data('selection-mode'),
-        selected       = undefined,
-        icons          = {
-          column_show: "ui-icon-radio-off",
-          column_hide: "ui-icon-radio-on"
-        };
-
-    /* Modules */
-
-    /* localStorage column visibility module */
-    var column_visibility = undefined;
-    if (typeof localStorage !== 'undefined')
-      column_visibility = {
+  /* localStorage column visibility module */
+  var column_visibility = undefined;
+  if (typeof localStorage !== 'undefined')
+    column_visibility = cache_module(function (dyntbl_id) {
+      return {
         /* current column visibility status for this table */
         columns: {},
 
@@ -96,168 +82,23 @@
           return this.columns;
         }
       };
+    });
 
-    /* Requests */
+  /*
+   * +refresh+ event
+   * Present on .dynamic-table nodes, refreshes the dynamic table's UI elements.
+   * Trigger this event if the table's dynamic elements (rows, column visibility,
+   * filters, etc.) have changed.
+   */
+  $(document).delegate('.dynamic-table', 'refresh.dyn-tbl', function () {
 
-    /* trigger a sorting request when the header of a sortable column is clicked */
-    table.find('.dt-sort > .dt-hdr')
-      .unbind('click.dyn-tbl')
-      .bind('click.dyn-tbl', function () {
-        $(this)
-          .siblings('.dt-sort-btn')
-          .first()
-          .click();
-      });
-
-    /* sorting, filtering and pagination requests */
-    container.find('.dt-sort-btn, .dt-fpop-txt:not(.dt-zero), .dt-pag-pages > a')
-      .unbind('click.dyn-tbl')
-      .bind('click.dyn-tbl', function (event) {
-        event.preventDefault();
-
-        var url = $(this).attr('href') || $(this).data('url');
-        if (!url || !request_type) return;
-
-        switch (request_type) {
-        case 'html_link':
-          window.location.href = url;
-          break;
-
-        case 'ajax_replace':
-          $.get(url, function (data) {
-            container.replaceWith(data);
-            $('.dynamic-table').trigger('reload.dyn-tbl');
-          });
-          break;
-
-        case 'server_javascript':
-          $.get(url, $.noop, 'script');
-          break;
-
-        default:
-          return;
-        }
-      });
-
-    /* Popups */
-
-    /* show/hide popups on filter/columns display button clicks */
-    var popup_buttons = table.find('.dt-filter-btn, .dt-col-btn');
-    popup_buttons
-      .unbind('click.dyn-tbl')
-      .bind('click.dyn-tbl', function () {
-        popup_buttons
-          .not(this)
-          .removeClass('dt-shown')
-          .siblings('.dt-popup')
-          .hide();
-
-        var popup = $(this)
-          .siblings('.dt-popup')
-          .toggle();
-
-        $(this).toggleClass('dt-shown');
-
-        popup.trigger(popup.is(':visible') ? 'show.dyn-tbl' : 'hide.dyn-tbl');
-      });
-
-    /* hide popups when their close buttons are clicked */
-    table.find('.dt-pop-close-btn')
-      .unbind('click.dyn-tbl')
-      .bind('click.dyn-tbl', function () {
-        var popup = $(this)
-          .parent()
-          .hide();
-
-        popup
-          .siblings('.dt-filter-btn, .dt-col-btn')
-          .removeClass('dt-shown');
-
-        popup.trigger('hide');
-      });
-
-    /* stick the columns display popup in place when shown */
-    table.find('.dt-col-csp > .dt-popup')
-      .unbind('show.dyn-tbl')
-      .bind('show.dyn-tbl', function () {
-        var popup    = $(this),
-            width    = popup.outerWidth(),
-            edge_gap = 10,
-            position = popup.offset();
-
-        popup.parent().css({ position: 'static' });
-        popup.offset({
-          top:  position.top,
-          left: Math.min(position.left, $(window).width() - width - edge_gap)
-        });
-      })
-      .unbind('hide.dyn-tbl')
-      .bind('hide.dyn-tbl', function () {
-        $(this)
-          .css({ left: '', top: '' })
-          .parent()
-          .css({ position: 'relative' });
-      });
-
-    /* Selection */
-
-    /* show rows as selected if their respective checkbox is checked */
-    var checkboxes = table.find('td.dt-sel > .dt-sel-check');
-    checkboxes
-      .unbind('change.dyn-tbl')
-      .bind('change.dyn-tbl', function () {
-        $(this)
-          .closest('tr')
-          .toggleClass('dt-selected', $(this).prop('checked'));
-
-        if (selection_mode != 'single') return;
-
-        /* in single select mode, when a checkbox is checked, the last one gets unchecked */
-        if (selected && selected != this)
-          $(selected)
-            .prop('checked', false)
-            .closest('tr')
-            .removeClass('dt-selected');
-
-        selected = (selected == this ? undefined : this);
-      });
-
-    /* trigger selection checkboxes when the row is clicked */
-    table.find('.dt-body > .dt-sel-row')
-      .unbind('click.dyn-tbl')
-      .bind('click.dyn-tbl', function (event) {
-        if (!$(this).children('td').is(event.target)) return;
-
-        var checkbox = $(this)
-          .find('.dt-sel-check')
-          .first();
-
-        checkbox
-          .prop('checked', !checkbox.prop('checked'))
-          .trigger('change');
-      });
-
-    /* in single select mode, there is no need for a header checkbox */
-    if (selection_mode == 'single')
-      table.find('th.dt-sel')
-        .css({ visibility: 'hidden' });
-
-    /* toggle all checkboxes when the header checkbox is clicked */
-    table.find('th.dt-sel > .dt-sel-check')
-      .unbind('change.dyn-tbl')
-      .bind('change.dyn-tbl', function () {
-        var checked = $(this).prop('checked');
-
-        checkboxes
-          .prop('checked', checked)
-          .trigger('change');
-      });
-
-    /* Column visibility */
+    var dyntbl    = $(this),
+        dyntbl_id = dyntbl.attr('id'),
+        table     = dyntbl.find('.dt-table');
 
     /* restore previous column visibility status */
     if (column_visibility) {
-      var visibility = column_visibility.load();
+      var visibility = column_visibility(dyntbl_id).load();
 
       table.find('.dt-head > tr > th').each(function () {
         var column  = $(this).data('column'),
@@ -285,10 +126,195 @@
         .toggleClass('dt-hidden', hidden);
     });
 
+    adjust_empty(table);
+
+  });
+
+  /*
+   * +reload+ event
+   * Present on .dynamic-table nodes, reload the dynamic table's state and
+   * event bindings. Trigger this event if the table's static elements (headers,
+   * pagination, columns, etc.) have changed and the current state is no longer
+   * valid.
+   */
+  $(document).delegate('.dynamic-table', 'reload.dyn-tbl', function () {
+
+    var dyntbl         = $(this),
+        dyntbl_id      = dyntbl.attr('id'),
+        table          = dyntbl.find('.dt-table'),
+        request_type   = dyntbl.data('request-type'),
+        selection_mode = dyntbl.data('selection-mode'),
+        selected       = undefined;
+
+    /* Requests */
+
+    /* trigger a sorting request when the header of a sortable column is clicked */
+    dyntbl
+      .undelegate('.dt-sort > .dt-hdr', 'click.dyn-tbl')
+      .delegate('.dt-sort > .dt-hdr', 'click.dyn-tbl', function () {
+        $(this)
+          .siblings('.dt-sort-btn')
+          .first()
+          .click();
+      });
+
+    /* sorting, filtering and pagination requests */
+    dyntbl
+      .undelegate('.dt-sort-btn, .dt-fpop-txt:not(.dt-zero), .dt-pag-pages > a', 'click.dyn-tbl')
+      .delegate('.dt-sort-btn, .dt-fpop-txt:not(.dt-zero), .dt-pag-pages > a', 'click.dyn-tbl', function (event) {
+        event.preventDefault();
+
+        var url = $(this).attr('href') || $(this).data('url');
+        if (!url || !request_type) return;
+
+        switch (request_type) {
+        case 'html_link':
+          window.location.href = url;
+          break;
+
+        case 'ajax_replace':
+          $.get(url, function (data) {
+            dyntbl.replaceWith(data);
+            $('.dynamic-table').trigger('reload.dyn-tbl');
+          });
+          break;
+
+        case 'server_javascript':
+          $.get(url, $.noop, 'script');
+          break;
+
+        default:
+          return;
+        }
+      });
+
+    /* Popups */
+
+    /* show/hide popups on filter/columns display button clicks */
+    dyntbl
+      .undelegate('.dt-filter-btn, .dt-col-btn', 'click.dyn-tbl')
+      .delegate('.dt-filter-btn, .dt-col-btn', 'click.dyn-tbl', function () {
+        table
+          .find('.dt-filter-btn, .dt-col-btn')
+          .not(this)
+          .removeClass('dt-shown')
+          .siblings('.dt-popup')
+          .hide();
+
+        var popup = $(this)
+          .siblings('.dt-popup')
+          .toggle();
+
+        $(this).toggleClass('dt-shown');
+
+        popup.trigger(popup.is(':visible') ? 'show.dyn-tbl' : 'hide.dyn-tbl');
+      });
+
+    /* hide popups when their close buttons are clicked */
+    dyntbl
+      .undelegate('.dt-pop-close-btn', 'click.dyn-tbl')
+      .delegate('.dt-pop-close-btn', 'click.dyn-tbl', function () {
+        var popup = $(this)
+          .parent()
+          .hide();
+
+        popup
+          .siblings('.dt-filter-btn, .dt-col-btn')
+          .removeClass('dt-shown');
+
+        popup.trigger('hide.dyn-tbl');
+      });
+
+    /* stick the columns display popup in place when shown */
+    dyntbl
+      .undelegate('.dt-col-csp > .dt-popup', 'show.dyn-tbl')
+      .delegate('.dt-col-csp > .dt-popup', 'show.dyn-tbl', function () {
+        var popup    = $(this),
+            width    = popup.outerWidth(),
+            edge_gap = 10,
+            position = popup.offset();
+
+        popup.parent().css({ position: 'static' });
+        popup.offset({
+          top:  position.top,
+          left: Math.min(position.left, $(window).width() - width - edge_gap)
+        });
+      })
+      .undelegate('.dt-col-csp > .dt-popup', 'hide.dyn-tbl')
+      .delegate('.dt-col-csp > .dt-popup', 'hide.dyn-tbl', function () {
+        $(this)
+          .css({ left: '', top: '' })
+          .parent()
+          .css({ position: 'relative' });
+      });
+
+    /* Selection */
+
+    /* show rows as selected if their respective checkbox is checked */
+    dyntbl
+      .undelegate('td.dt-sel > .dt-sel-check', 'change.dyn-tbl')
+      .delegate('td.dt-sel > .dt-sel-check', 'change.dyn-tbl', function () {
+        $(this)
+          .closest('tr')
+          .toggleClass('dt-selected', $(this).prop('checked'));
+
+        if (selection_mode != 'single') return;
+
+        /* in single select mode, when a checkbox is checked, the last one gets unchecked */
+        if (selected && selected != this)
+          $(selected)
+            .prop('checked', false)
+            .closest('tr')
+            .removeClass('dt-selected');
+
+        selected = (selected == this ? undefined : this);
+      });
+
+    /* trigger selection checkboxes when the row is clicked */
+    dyntbl
+      .undelegate('.dt-body > .dt-sel-row', 'click.dyn-tbl')
+      .delegate('.dt-body > .dt-sel-row', 'click.dyn-tbl', function (event) {
+        if (!$(this).children('td').is(event.target)) return;
+
+        var checkbox = $(this)
+          .find('.dt-sel-check')
+          .first();
+
+        checkbox
+          .prop('checked', !checkbox.prop('checked'))
+          .trigger('change');
+      });
+
+    /* in single select mode, there is no need for a header checkbox */
+    if (selection_mode == 'single')
+      table.find('th.dt-sel')
+        .css({ visibility: 'hidden' });
+
+    /* toggle all checkboxes when the header checkbox is clicked */
+    dyntbl
+      .undelegate('th.dt-sel > .dt-sel-check', 'change.dyn-tbl')
+      .delegate('th.dt-sel > .dt-sel-check', 'change.dyn-tbl', function () {
+        var checked = $(this).prop('checked');
+
+        if (selection_mode == 'single') return;
+
+        /*
+         * FIXME: while triggering 'change' for every checkbox is the correct
+         * behavior to implement, it is also extremely slow...
+         */
+        table
+          .find('td.dt-sel > .dt-sel-check')
+          .prop('checked', checked)
+          .closest('tr')
+          .toggleClass('dt-selected', checked);
+      });
+
+    /* Column visibility */
+
     /* toggle columns when the column is clicked in the column display popup */
-    table.find('.dt-cpop-col')
-      .unbind('click.dyn-tbl')
-      .bind('click.dyn-tbl', function () {
+    dyntbl
+      .undelegate('.dt-cpop-col', 'click.dyn-tbl')
+      .delegate('.dt-cpop-col', 'click.dyn-tbl', function () {
         $(this)
           .find('.dt-cpop-icon')
           .toggleClass(icons.column_show)
@@ -301,21 +327,21 @@
           .find(['td.' + column, 'th.' + column].join(','))
           .toggleClass('dt-hidden');
 
-        adjust_empty();
+        adjust_empty(table);
 
         /* update the column's visibility status */
         if (column_visibility) {
-          column_visibility.columns[column] = table.find('th.' + column).is(':visible');
-          column_visibility.save();
+          column_visibility(dyntbl_id).columns[column] = table.find('th.' + column).is(':visible');
+          column_visibility(dyntbl_id).save();
         }
       });
 
     /* Misc */
 
     /* filter out filter options if they dont begin with the search input */
-    table.find('.dt-fpop-find > input')
-      .unbind('input.dyn-tbl')
-      .bind('input.dyn-tbl', function () {
+    dyntbl
+      .undelegate('.dt-fpop-find > input', 'input.dyn-tbl')
+      .delegate('.dt-fpop-find > input', 'input.dyn-tbl', function () {
         var key = $.trim($(this).val()).toLowerCase();
 
         $(this)
@@ -333,21 +359,30 @@
           });
       });
 
-    /* adjust the width of table-wide cells */
-    function adjust_empty() {
-      var empty = table.find('.dt-body > tr > .dt-table-wide');
-      if (!empty.length) return;
-
-      empty.attr('colspan',
-        table
-          .find('.dt-head > tr > th:not(.dt-col-csp):not(.dt-hidden)')
-          .length
-      );
-    };
-
-    adjust_empty();
+    dyntbl.trigger('refresh.dyn-tbl');
 
   });
+
+  /* adjust the width of table-wide cells for +table+ */
+  function adjust_empty(table) {
+    var empty = table.find('.dt-body > tr > .dt-table-wide');
+    if (!empty.length) return;
+
+    empty.attr('colspan',
+      table
+        .find('.dt-head > tr > th:not(.dt-col-csp):not(.dt-hidden)')
+        .length
+    );
+  };
+
+  /* cache a table-specific (one per dynamic table) +module+ */
+  function cache_module(module) {
+    var tables = {};
+
+    return function (id) {
+      return tables[id] || (tables[id] = module(id));
+    };
+  };
 
   /* load the table at initial page load */
   $('.dynamic-table').trigger('reload.dyn-tbl');
