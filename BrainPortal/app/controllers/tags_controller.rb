1849de2ca5c298916752a1ea80fe71bf612c2546
Ehsan Afkhami <eafkhami@dhcp56-200.mni.mcgill.ca>
2015-10-22 12:17:04 -0400

    updated fix for issue #209 and #241

--- BrainPortal/app/controllers/tags_controller.rb
+++ BrainPortal/app/controllers/tags_controller.rb
@@ -1,4 +1,3 @@
-
 #
 # CBRAIN Project
 #
@@ -26,6 +25,7 @@ class TagsController < ApplicationController
   Revision_info=CbrainFileRevision[__FILE__] #:nodoc:
 
   before_filter :login_required
+  before_filter(:only => [:update]) { |c| c.authenticate_params }
   layout false
 
   def new #:nodoc:
@@ -43,7 +43,7 @@ class TagsController < ApplicationController
     @tag = Tag.new(params[:tag])
 
      @tag.user_id = current_user.id if @tag.user_id.blank? || !current_user.available_users.raw_first_column(:user_id).include?(@tag.user_id)
-     @tag.group_id = current_user.own_group.id if @tag.user_id.blank? || !current_user.available_groups.map(&:id).include?(@tag.group_id)
+     @tag.group_id = current_user.own_group.id if @tag.group_id.blank? || !current_user.available_groups.map(&:id).include?(@tag.group_id)
 
     respond_to do |format|
       if @tag.save
@@ -62,9 +62,6 @@ class TagsController < ApplicationController
   # PUT /tags/1.xml
   def update #:nodoc:
     @tag = current_user.tags.find(params[:id])
-
-    @tag.user_id = current_user.id if @tag.user_id.blank? || !current_user.available_users.raw_first_column(:user_id).include?(@tag.user_id)
-    @tag.group_id = current_user.own_group.id if @tag.user_id.blank? || !current_user.available_groups.map(&:id).include?(@tag.group_id)
 
     respond_to do |format|
       if @tag.update_attributes(params[:tag])
@@ -94,4 +91,17 @@ class TagsController < ApplicationController
       format.xml  { head :ok }
     end
   end
+
+
+
+  def authenticate_params
+    user_id = params[:tag][:user_id]
+    group_id = params[:tag][:group_id]
+
+    params[:tag][:user_id] = current_user.id if user_id.blank? || !current_user.available_users.raw_first_column(:id).include?(user_id)
+    params[:tag][:group_id] = current_user.own_group.id if group_id.blank? || !current_user.available_groups.raw_first_column(:id).include?(group_id)
+  end
+
+
+
 end
