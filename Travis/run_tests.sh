8487f80bd5c29337ee85e2f91e0dde8e05b719f1
Pierre Rioux <pierre.rioux@mcgill.ca>
2016-11-09 20:50:40 -0500

    Re-engineered the Travis-CI testing environment.

---
+++ Travis/run_tests.sh
@@ -0,0 +1,64 @@
+#!/bin/bash --login
+
+set -a # bash will exit immediately if any command returns a code other than 0
+
+#####################
+# Utility functions #
+#####################
+
+# Prints a message and exits with a non-zero code.
+function die {
+    echo Fatal: "$*"
+    exit 2
+}
+
+###############
+# Main script #
+###############
+
+# Install the code base to be tested
+cd $HOME
+test -e "cbrain_test" && ls -la && die "Oh oh, some directory 'cbrain_test' is in the way"
+cp -p -r cbrain_travis cbrain_test || die "Cannot copy the cbrain code base to 'cbrain_test'..."
+
+# Edits DB configuration file from template
+dockerize -template $HOME/cbrain_test/Travis/templates/database.yml.TEMPLATE:$HOME/cbrain_test/BrainPortal/config/database.yml                      || die "Cannot edit DB configuration file"
+
+# Edits portal name from template
+dockerize -template $HOME/cbrain_test/Travis/templates/config_portal.rb.TEMPLATE:$HOME/cbrain_test/BrainPortal/config/initializers/config_portal.rb || die "Cannot edit CBRAIN configuration file"
+
+# Waits for DB to be available
+dockerize -wait tcp://${MYSQL_HOST}:${MYSQL_PORT} -timeout 90s || die "Cannot wait for mysql:3306 to be up or timeout was reached"
+
+# Runs a simple query to make sure we can access the DB.
+mysql_options="-h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USER} --password=${MYSQL_PASSWORD}"
+for cnt in 1 2 3 4 5 6 7 8 9 10 waytoolong
+do
+  echo "Attempt #$cnt : $(date) - trying to connect to the database"
+  mysql $mysql_options -e 'select "Yippee, database is now accessible";' && break
+  test "X$cnt" = "Xwaytoolong" && die "Cannot wait for DB any longer..."
+  sleep 3
+done
+
+# Prepare the DB for testing.
+export RAILS_ENV=test
+
+# Make sure RVM is loaded
+source /home/cbrain/.bashrc
+
+# Go to the new code to test
+cd $HOME/cbrain_test/BrainPortal  || die "Cannot cd to BrainPortal directory"
+
+# Prep all that needs to be prepared. With a bit of luck, bundle install
+# will be quite quick given that in the dicker image we already did it
+# in ~/cbrain_base .
+bundle install
+rake cbrain:plugins:install:plugins || die "Cannot install cbrain:plugins"
+rake db:schema:load                 || die "Cannot load DB schema"
+rake db:seed                        || die "Cannot seed DB"
+rake db:sanity:check                || die "Cannot sanity check DB"
+
+# Eventually, it would be nice if from a ENV variable set in Travis,
+# we could run only a subset of the tests.
+rspec spec                          || die "Failed running rspec"
+
--- Travis/travis_ci.sh
+++ Travis/travis_ci.sh
@@ -2,16 +2,21 @@
 
 ###############################################################################
 #                                                                             #
-# This script is used by travis (https://travis-ci.org/) to build and test    #
-# CBRAIN when a Pull Request is done.                                         #
+# This script is used by Travis CI (https://travis-ci.org/) to run the        #
+# CBRAIN test suite.                                                          #
 #                                                                             #
-# This script do the following:                                               #
-#   - Create some docker containers to run the testing suite.                 #
-#   - Run the testing suit inside the cbrain_portal container.                #
-#   - If all the test pass then travis will pass otherwise it will fail.      #
+# The script expects a testing docker container to already have been built    #
+# and made available from the local system. The name of that docker image is  #
+# expected in the environement variable $IMAGE_NAME, or given as a first      #
+# argument.                                                                   #
+#                                                                             #
+# This script does the following:                                             #
+#   - Invoke the container as part of a docker-compose setup, which           #
+#     links the image to a MariaDB service.                                   #
+#   - Run the testing suite inside the container.                             #
+#   - Returns the return code (and possibly diagnostics) of the suite.        #
 #                                                                             #
 ###############################################################################
-
 
 # Terminal colors, using ANSI sequences.
 RED='\033[31m'
@@ -19,38 +24,41 @@ GREEN='\033[32m'
 MAGENTA='\033[35m'
 NC='\033[0m'
 
-# Do we even have a Docker environment set up ?
-cd Travis
-if [ $? -ne 0 ] ; then
+# Do we even have a Travis+Docker environment set up ?
+if test ! -d Travis ; then
   printf "${RED}No 'Travis' subdirectory found.${NC}\n"
   exit 2
 fi
+cd Travis
 
-# Construction of docker containers
-printf "${MAGENTA} Building docker containers.${NC}\n"
-bash build.sh
-if [ $? -ne 0 ] ; then
-  printf "${RED}Construction of docker containers failed.${NC}\n"
-  exit 5
+# Do we have a docker image name to run?
+IMAGE_NAME=${IMAGE_NAME:-$1} # can be given as argument
+if test "X$IMAGE_NAME" = "X" ; then
+  printf "${RED}No IMAGE_NAME environment variable supplied.${NC}\n"
+  exit 2
 fi
 
-COMPOSE_PROJECT_NAME='travis'
+# Count time
+SECONDS=0 # bash is great
 
 # Run the docker containers
-printf "${MAGENTA} Running docker containers.${NC}\n"
-env USERID=500 GROUPID=500 docker-compose -p 'travis' up -d
+printf "${MAGENTA}Running containers in Docker Compose.${NC}\n"
+export COMPOSE_PROJECT_NAME='travis'
+env IMAGE_NAME=$IMAGE_NAME docker-compose -p $COMPOSE_PROJECT_NAME up -d
 if [ $? -ne 0 ] ; then
-  printf "${RED}Docker Compose Failed${NC}\n"
+  printf "${RED}Docker Compose Failed. So sorry.${NC}\n"
   exit 10
 fi
-TEST_EXIT_CODE=`docker wait travis_cbrain-portal_1`
+test_exit_code=`docker wait travis_cbrain_1`
+printf "${MAGENTA}Docker Compose finished after $SECONDS seconds.${NC}\n"
 
 # Final Results
-if [ -z "${TEST_EXIT_CODE}" ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then
-  printf "${RED}Tests Failed${NC} - 'docker wait' exit code: $TEST_EXIT_CODE\n"
-  docker logs travis_cbrain-portal_1
+if [ "X$test_exit_code" != "X0" ] ; then
+  printf "${RED}Tests Failed${NC} - 'docker wait' exit code: $test_exit_code\n"
+  docker logs travis_cbrain_1
   exit 20
 fi
 
-printf "${GREEN}Tests Passed${NC}\n"
+printf "${GREEN}All tests Passed${NC}\n"
 exit 0
+
