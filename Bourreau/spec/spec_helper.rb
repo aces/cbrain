5abeecef655539c54b169655cd6b3c689cfb170c
Pierre Rioux <pierre.rioux@mcgill.ca>
2017-04-21 19:52:33 -0400

    Added missing FactoryGirl require.

--- Bourreau/spec/spec_helper.rb
+++ Bourreau/spec/spec_helper.rb
@@ -17,6 +17,9 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'factory_girl_rails'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
