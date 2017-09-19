f1313b2fad911425b7dee7f0686510892d46deae
Greg Kiar <gkiar07@gmail.com>
2017-09-19 14:16:48 -0400

    added save to migration

--- BrainPortal/db/migrate/20170913185428_add_container_index_to_tool_configs.rb
+++ BrainPortal/db/migrate/20170913185428_add_container_index_to_tool_configs.rb
@@ -3,10 +3,11 @@ class AddContainerIndexToToolConfigs < ActiveRecord::Migration
     add_column :tool_configs, :container_index_location, :string
     ToolConfig.find_each do |tool|
       if tool.container_engine == "Singularity" && tool.containerhub_image_name.present?
-        tmp = tool.containerhub_image_name.split('://') # Splits container image name based on singularity expected syntax
+        tmp = tool.containerhub_image_name.split("://") # Splits container image name based on singularity expected syntax
         tool.containerhub_image_name = tmp[-1] # The container will always be the last item in the list
-        tool.container_index_location = tmp[0] + "://" if tmp.length == 2 # The index location will be default if none found, else the first element
+        tool.container_index_location = tmp[0] + "://" if tmp.length >= 2 # The index location will be default if none found, else the first element
       end
+      tool.save
     end
   end
 
@@ -15,6 +16,7 @@ class AddContainerIndexToToolConfigs < ActiveRecord::Migration
       if tool.container_engine == "Singularity" && tool.container_index_location.present?
         tool.containerhub_image_name = tool.container_index_location + tool.containerhub_image_name
       end
+      tool.save
     end
     remove_column :tool_configs, :container_index_location
   end
