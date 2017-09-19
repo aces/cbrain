1086f42efbbbd9c1dd763a3fcee4b31b384a6918
Greg Kiar <gkiar07@gmail.com>
2017-09-19 14:11:21 -0400

    added reversible migration for singularity containers with index (@prioux)

--- BrainPortal/db/migrate/20170913185428_add_container_index_to_tool_configs.rb
+++ BrainPortal/db/migrate/20170913185428_add_container_index_to_tool_configs.rb
@@ -1,5 +1,21 @@
 class AddContainerIndexToToolConfigs < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :tool_configs, :container_index_location, :string
+    ToolConfig.find_each do |tool|
+      if tool.container_engine == "Singularity" && tool.containerhub_image_name.present?
+        tmp = tool.containerhub_image_name.split('://') # Splits container image name based on singularity expected syntax
+        tool.containerhub_image_name = tmp[-1] # The container will always be the last item in the list
+        tool.container_index_location = tmp[0] + "://" if tmp.length == 2 # The index location will be default if none found, else the first element
+      end
+    end
+  end
+
+  def self.down
+    ToolConfig.find_each do |tool|
+      if tool.container_engine == "Singularity" && tool.container_index_location.present?
+        tool.containerhub_image_name = tool.container_index_location + tool.containerhub_image_name
+      end
+    end
+    remove_column :tool_configs, :container_index_location
   end
 end
