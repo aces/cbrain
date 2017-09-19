5c1ccb1b9b0b71540d6f2a599c45c8e5d1720635
Greg Kiar <gkiar07@gmail.com>
2017-09-19 12:45:21 -0400

    improved testing for singularity and docker container indices with informative error messages

--- BrainPortal/app/models/tool_config.rb
+++ BrainPortal/app/models/tool_config.rb
@@ -56,9 +56,6 @@ class ToolConfig < ActiveRecord::Base
                                    :message => "must be unique per pair [tool, server]" },
                   :if         => :applies_to_bourreau_and_tool?
 
-  validates                 :container_index_location,
-                            :format => { :with => /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}$|^shub\:\/\/$/},
-                            :allow_blank => true
 
   validate        :container_rules
 
@@ -342,7 +339,18 @@ class ToolConfig < ActiveRecord::Base
     if self.container_engine.present? && ( self.containerhub_image_name.blank? && self.container_image_userfile_id.blank? )
       errors[:container_engine] = "a container hub image name or a container image userfile ID should be set when the container engine is set"
     end
+
+    if self.container_engine.present? && self.container_engine == "Singularity" 
+      if self.container_index_location.present? && ! /^[a-z0-9]+\:\/\/$/.match(self.container_index_location)
+        errors[:container_index_location] = "Invalid index location type for container engine Singularity. Should end in '://'."
+      end
+    elsif self.container_engine.present? && self.container_engine == "Docker"
+      if self.container_index_location.present? && ! /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}$/.match(self.container_index_location)
+        errors[:container_index_location] = "Invalid index location type for container engine Docker. Should be a valid hostname."
+      end
+    end
     return errors.empty?
+
   end
 
 
