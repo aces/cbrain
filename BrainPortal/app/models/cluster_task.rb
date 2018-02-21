5e7ae9996e0533db4f84d26e46608c194496c2a2
Natacha Beck <natabeck@gmail.com>
2018-02-21 13:26:25 -0500

    Ignore error when building singularity image

--- BrainPortal/app/models/cluster_task.rb
+++ BrainPortal/app/models/cluster_task.rb
@@ -2406,15 +2406,12 @@ chmod o+x . .. ../.. ../../..
       # of these blocks can be scheduled to run, but only one will execute at at any given time.
       next if File.exists?(cache_path.to_s) && File.size(cache_path.to_s) > 0
       # Run singularity build command
-      out,err = tool_config_system("umask 002; #{singularity_executable_name} build #{cache_path.to_s.bash_escape} #{singularity_index_location.bash_escape}#{singularity_image_name.bash_escape}")
+      tool_config_system("umask 002; #{singularity_executable_name} build #{cache_path.to_s.bash_escape} #{singularity_index_location.bash_escape}#{singularity_image_name.bash_escape}")
       # Singularity command can generate 'implausibly old time stamp' when pulling a docker image (due to tar), we ignore it.
       # Not sure if it is still true for `build` command, leave it just in case.
       # Remove all lines (use ^ and $) that contains this message.
-      err.gsub!(/^.*implausibly old time stamp.*$\n?/,"")
-      err.gsub!(/^.*WARNING:.+/, "")
       cb_error "Cannot build singularity image" if
-        err.present? ||
-        ! File.exists?(cache_path.to_s)
+        File.zero?(cache_path.to_s)
     end
 
     self.addlog("Image stored as scratch userfile '#{scratch_userfile.name}' (ID #{scratch_userfile.id})")
