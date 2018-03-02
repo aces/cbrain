e5f27ff85b66029de066a61cb649048ee587fda2
Natacha Beck <natabeck@gmail.com>
2018-03-02 14:20:04 -0500

    Update cluster_task.rb

--- BrainPortal/app/models/cluster_task.rb
+++ BrainPortal/app/models/cluster_task.rb
@@ -1971,7 +1971,7 @@ exit $status
     userfile_name   = "TaskRawWorkdir-#{self.tname_run_id}"
     file_collection = safe_userfile_find_or_new(TaskRawWorkdir,
         :name             => userfile_name,
-        :data_provider_id => data_provider_id
+        :data_provider_id => data_provider_id,
         :user_id          => user_id
       )
 
