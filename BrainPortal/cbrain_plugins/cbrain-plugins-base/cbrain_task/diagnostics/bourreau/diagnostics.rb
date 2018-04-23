e4f0bd8861c28db5ebd68beede4dd43ecb7603b0
Pierre Rioux <pierre.rioux@mcgill.ca>
2018-04-23 13:05:31 -0400

    Diagnostics task has new option
    Boolean option: when restarting at PostProcess, we can
    ask for a perfect copy of the previous run's captured
    output and error to be used as if they were created in
    the new run.

--- BrainPortal/cbrain_plugins/cbrain-plugins-base/cbrain_task/diagnostics/bourreau/diagnostics.rb
+++ BrainPortal/cbrain_plugins/cbrain-plugins-base/cbrain_task/diagnostics/bourreau/diagnostics.rb
@@ -408,14 +408,28 @@ class CbrainTask::Diagnostics < ClusterTask
       sleep params[:restart_postpro_delay].to_i
     end
 
+    # In most case, that's it. We have an option top copy the
+    # the out and err files of the previous run if we want, for
+    # debugging outer things.
+    return true unless mybool(params[:restart_postpro_copy_outerr])
+
     # We simply copy the out and err of the previous run when we restart at post-pro.
-    cur_out = self.stdout_cluster_filename
-    cur_err = self.stderr_cluster_filename
-    if File.exists?(cur_out)
-      system("cp",cur_out,self.stdout_cluster_filename(self.run_number + 1))
+    # There are up to four files to copy... :-(
+    qsub_out = qsub_stdout_basename(    self.run_number )
+    qsub_err = qsub_stderr_basename(    self.run_number )
+    sci_out  = science_stdout_basename( self.run_number )
+    sci_err  = science_stderr_basename( self.run_number )
+    if File.exists?(qsub_out)
+      system("cp",qsub_out,qsub_stdout_basename(self.run_number + 1))
     end
-    if File.exists?(cur_err)
-      system("cp",cur_err,self.stderr_cluster_filename(self.run_number + 1))
+    if File.exists?(qsub_err)
+      system("cp",qsub_err,qsub_stderr_basename(self.run_number + 1))
+    end
+    if File.exists?(sci_out)
+      system("cp",sci_out,science_stdout_basename(self.run_number + 1))
+    end
+    if File.exists?(sci_err)
+      system("cp",sci_err,science_stderr_basename(self.run_number + 1))
     end
     return true
   end
