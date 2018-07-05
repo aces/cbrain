52426f6b90dd5e87cef22bb1aeecce68a9dcb791
MontrealSergiy <sergeboroday@gmail.com>
2018-07-05 14:41:37 -0400

    fix #750 Links to ExceptionLog objects don't work anymore

--- BrainPortal/app/helpers/show_table_helper.rb
+++ BrainPortal/app/helpers/show_table_helper.rb
@@ -321,9 +321,12 @@ module ShowTableHelper
   #  table is editable (+edit_condition+ is specified as true), an edit toggle
   #  will be added next to the header/title to switch into edition mode.
   def show_table(object, options = {}, &block)
-
+    tb_options = options.merge(block: block)
     url = options[:url].presence
-    if url.blank? && object.is_a?(ApplicationRecord)
+    # '', ' '... should be interpreted as false (non-editable for some reason)
+    tb_options[:edit_condition] = edit_condition = options[:edit_condition].presence
+
+    if url.blank? && object.is_a?(ApplicationRecord) && edit_condition
       url = { :controller  => params[:controller] }
       if object.new_record?
         url[:action] = :create
@@ -331,10 +334,10 @@ module ShowTableHelper
         url[:action] = :update
         url[:id]     = object.id
       end
-      url = url_for(url)
+      tb_options[:url] = url_for(url)
     end
 
-    tb = TableBuilder.new(object, self, options.merge(:block => block, :url => url))
+    tb = TableBuilder.new(object, self, tb_options)
 
     render :partial => 'shared/show_table', :locals => { :tb => tb }
   end
