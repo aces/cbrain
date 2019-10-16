b90fd343222cd4a5be2306724a338e8f16ecbf4f
Pierre Rioux <pierre.rioux@mcgill.ca>
2019-10-16 13:14:43 -0400

    Added more runtime tracking info for tasks.
    Also made the text report prettier.

--- BrainPortal/vendor/cbrain/bin/runtime_info.sh
+++ BrainPortal/vendor/cbrain/bin/runtime_info.sh
@@ -17,7 +17,7 @@
 # will change the key part to
 #   Some_name = some value
 function underscore_keys() {
-  perl -pe 's/^\s*(.*?\S)\s*[:=]/ $x=$1; $x =~ s#\s+#_#g; "$x="/e'
+  perl -pe 's/^\s*(.*?\S)\s*[:=]/ $x=$1; $x =~ s#\s+#_#g; "$x="/e; $_="" if /=\s*$|^\s*$/'
 }
 
 # Header
@@ -31,27 +31,53 @@ echo "#"
 echo ""
 
 # Internal version tracking
-echo "runtime_info_version = $version"
+echo "cbrain_runtime_info_version = $version"
 
 # Basic UNIX stuff
 echo "hostname =" $(hostname)
-echo ""
+if test -n "$(type -p singularity)" ; then
+  echo ""
+  echo "singularity_version =" $(singularity version 2>/dev/null | head -1)
+fi
+
+if test -n "$(type -p docker)" ; then
+  echo ""
+  echo "docker_version =" $(docker -v 2>/dev/null | head -1)
+fi
 
 # LINUX: os-release (all systemd systems have that)
 if test -f /etc/os-release ; then
-  cat /etc/os-release | underscore_keys
+  echo ""
+  echo "# From /etc/os-release"
+  cat /etc/os-release            | \
+    grep -v _URL                 | \
+    grep -v ANSI_COLOR           | \
+    underscore_keys              | \
+    sed -e  's/^ */os_release_/'
 fi
 
 # LINUX: cpuinfo
 if test -e /proc/cpuinfo ; then
-  cat /proc/cpuinfo | sort | uniq | \
+  echo ""
+  echo "# From /proc/cpuinfo"
+  cat /proc/cpuinfo              | \
+  sort                           | \
+  uniq                           | \
   egrep '^(vendor_id|flags|cache size|cpu cores|model name|microcode)' | \
+  sed -e  's/^ */proc_cpuinfo_/' | \
   underscore_keys
 fi
 
-# MacOS
-sw_vers 2>/dev/null | underscore_keys
+# MacOS stuff
+if test -n "$(type -p sw_vers)" ; then
+  echo ""
+  echo "# From sw_vers"
+  sw_vers 2>/dev/null | underscore_keys
+fi
 
-# MacOS
-system_profiler SPSoftwareDataType SPHardwareDataType 2>/dev/null | underscore_keys
+if test -n "$(type -p system_profiler)" ; then
+  echo ""
+  echo "# From system_profiler"
+  system_profiler SPSoftwareDataType SPHardwareDataType 2>/dev/null | underscore_keys
+fi
 
