372ce1d0f3fed2a23bdf5b022557509a599dbc5d
candicecz <czech.candice@gmail.com>
2020-05-27 11:18:47 -0400

    [FIX] issue #931: Add Public Project to CBRAIN

--- BrainPortal/app/models/work_group.rb
+++ BrainPortal/app/models/work_group.rb
@@ -81,6 +81,8 @@ class WorkGroup < Group
     return @_pretty_category_name if @_pretty_category_name
     if self.invisible?
       @_pretty_category_name = 'Invisible Project'
+    elsif self.public?
+      @_pretty_category_name = 'Public Project'
     elsif self.users.count == 0
       @_pretty_category_name = 'Empty Work Project'
     elsif self.users.count > 1
@@ -94,6 +96,9 @@ class WorkGroup < Group
   end
 
   def short_pretty_type #:nodoc:
+    if self.public?
+      return "Public"
+    end
     if self.users.count > 1
       return "Shared"
     else
