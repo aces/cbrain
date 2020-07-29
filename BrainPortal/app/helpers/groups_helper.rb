0dc7a51c369ecb01b996228ae8bcd7efac0cb81c
Pierre Rioux <pierre.rioux@mcgill.ca>
2020-07-29 16:14:09 -0400

    Making things a bit prettier.

--- BrainPortal/app/helpers/groups_helper.rb
+++ BrainPortal/app/helpers/groups_helper.rb
@@ -50,7 +50,7 @@ module GroupsHelper
 
     center_legend(nil, groups.map { |g| css_group_type(g) }.uniq.map { |g|
       # 9675: UTF8 white circle, 9679: UTF8 black circle
-      ["<span class=\"#{g}_project_point\">&##{g == "all" ? 9675 : 9679};</span>", "#{g.titleize} Project"]
+      ["<span class=\"#{g}_project_point\">&##{g == "all" ? "x25ef" : "x2b24"};</span>", "#{g.titleize} Project"]
     })
   end
 end
