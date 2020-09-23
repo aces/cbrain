fba43a3b625d776d1afbc52714d0c6d103ba1eb3
Pierre Rioux <pierre.rioux@mcgill.ca>
2020-09-23 11:52:41 -0400

    ToolConfigs can have Singularity overlays to mount
    The admin can specify singularity overlays to mount.
    The overlays can be specific paths, or refer to
    all the overlays assocuated with a SingSquashfsDataProvider.
    
    The make_available() method will detect if a file is
    located on such an overlay and create its symlink to
    point direcly there without synchronizing.

---
+++ BrainPortal/db/migrate/20200922162525_add_singularity_overlays_specs_to_tool_configs.rb
@@ -0,0 +1,27 @@
+
+#
+# CBRAIN Project
+#
+# Copyright (C) 2020
+# The Royal Institution for the Advancement of Learning
+# McGill University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+class AddSingularityOverlaysSpecsToToolConfigs < ActiveRecord::Migration[5.0]
+  def change
+    add_column :tool_configs, :singularity_overlays_specs, :text
+  end
+end
--- BrainPortal/db/schema.rb
+++ BrainPortal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200804163321) do
+ActiveRecord::Schema.define(version: 20200922162525) do
 
   create_table "access_profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string   "name",        null: false
@@ -430,6 +430,7 @@ ActiveRecord::Schema.define(version: 20200804163321) do
     t.string   "containerhub_image_name"
     t.string   "container_engine"
     t.string   "container_index_location"
+    t.text     "singularity_overlays_specs",  limit: 65535
     t.index ["bourreau_id"], name: "index_tool_configs_on_bourreau_id", using: :btree
     t.index ["tool_id"], name: "index_tool_configs_on_tool_id", using: :btree
   end
