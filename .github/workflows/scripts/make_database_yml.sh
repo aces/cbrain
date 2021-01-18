63450d7493f8897d39c230faa40da91999e1a549
Pierre Rioux <pierre.rioux@mcgill.ca>
2021-01-18 16:04:39 -0500

    T17

--- .github/workflows/scripts/make_database_yml.sh
+++ .github/workflows/scripts/make_database_yml.sh
@@ -4,12 +4,12 @@
 # DB configuration for the test environment
 
 cat <<RAILS_DB_CONFIG_YML
-${RAILS_ENV:-test}:
+${RAILS_ENV:-bad_test}:
   adapter: mysql2
   host: 127.0.0.1
-  database: ${MARIADB_DATABASE:-cbrain_test}
-  username: ${MARIADB_USER:-cbrain_user}
-  password: ${MARIADB_PASSWORD:-no.such.thing}
-  port: ${MARIADB_PORT:-3306}
+  database: ${MARIADB_DATABASE:-bad_cbrain_test}
+  username: ${MARIADB_USER:-bad_cbrain_user}
+  password: ${MARIADB_PASSWORD:-bad_no_such_thing}
+  port: ${MARIADB_PORT:-13306}
 RAILS_DB_CONFIG_YML
 
