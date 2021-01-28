bdc8dc85d33a1d578a8555500df1939cdb419e23
MontrealSergiy <sergeboroday@gmail.com>
2021-01-28 16:26:44 -0500

    block all user cbcsv from public #1055

--- BrainPortal/app/controllers/userfiles_controller.rb
+++ BrainPortal/app/controllers/userfiles_controller.rb
@@ -1333,16 +1333,17 @@ class UserfilesController < ApplicationController
     # Create the new file list
     file_list = CbrainFileList.new(
       :user_id          => current_user.id,
-      :group_id         => current_assignable_group_id,
+      :group_id         => if current_project.try(:public) then current_user.own_group.id else current_assignable_group_id end,
       :name             => "file_list.#{Process.pid}.#{Time.now.to_i}.cbcsv",
       :data_provider_id => dest_dp_id,
-    )
+      )
 
     # Save it and set its content.
     if file_list.save
       csv_text = CbrainFileList.create_csv_file_from_userfiles(userfiles)
       file_list.cache_writehandle { |fh| fh.write(csv_text) }
-      flash[:notice] = "Created file list named '#{file_list.name}'."
+      flash[:notice] = "Created file list named '#{file_list.name}' in '#{file_list.group.name}' project."\
+                       " Make sure that it is what you need and, if needed, change the filename or project below."
       redirect_to(:controller => :userfiles, :action => :show, :id => file_list.id)
     else
       flash[:error] = "Could not create file list. Contact the admins."
