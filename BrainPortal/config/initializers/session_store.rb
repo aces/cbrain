4b61c48b800a5dde9d105044ae91c6be013d1746
Pierre Rioux <pierre.rioux@mcgill.ca>
2021-09-09 16:03:02 -0400

    Session cookies expire after 3 days.

--- BrainPortal/config/initializers/session_store.rb
+++ BrainPortal/config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: 'BrainPortal5_Session', same_site: :lax
+Rails.application.config.session_store :cookie_store,
+  :key          => 'BrainPortal5_Session',
+  :same_site    => :lax,
+  :expire_after => 3.days
