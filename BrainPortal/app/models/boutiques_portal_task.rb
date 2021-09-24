44a01a6f0941c824578dfceb060b9093e9c373f2
Pierre Rioux <pierre.rioux@mcgill.ca>
2021-09-24 14:45:37 -0400

    Shameful typo. Bad Pierre.

--- BrainPortal/app/models/boutiques_portal_task.rb
+++ BrainPortal/app/models/boutiques_portal_task.rb
@@ -282,7 +282,7 @@ class BoutiquesPortalTask < PortalTask
         f = Userfile.find_accessible_by_user( id, self.user, :access_requested => file_access_symbol() )
 
         # One task for that file
-        if ((! f.is_a?( CbrainFileList ) || input.list) # in case of a list input, we *do* assign it the CbFileList
+        if (! f.is_a?( CbrainFileList ) || input.list) # in case of a list input, we *do* assign it the CbFileList
           task = self.dup
           fillTask.( f, task )
 
