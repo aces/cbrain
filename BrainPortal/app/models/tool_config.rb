29ddaeed3ff8417c063f1025a02e959808fb61c2
MontrealSergiy <sergeboroday@gmail.com>
2021-12-01 18:01:14 -0500

    refactoring (formal overlay spec)

--- BrainPortal/app/models/tool_config.rb
+++ BrainPortal/app/models/tool_config.rb
@@ -342,7 +342,7 @@ class ToolConfig < ApplicationRecord
     # break onto commands based on comas, whitespace or comments
     specs = specs.split(/([\s,]|#.*$)+/).map(&:presence).compact
     specs.map do |spec|
-      # Full path or pattern
+      # Full path or pattern (legacy)
       next spec if spec =~ /^\// # just use that - FIXME remove after successful migration
       cb_error "Spec #{spec}  starts with semicolon" if spec.start_with?(':')
       knd, id_or_name = spec.split(':', 2)
@@ -377,7 +377,7 @@ class ToolConfig < ApplicationRecord
     return @_data_providers_with_overlays_ if @_data_providers_with_overlays_
     specs = self.singularity_overlays_specs.presence
     return [] if specs.blank?
-    specs = specs.split(/[\s,]+/).map(&:presence).compact
+    specs = specs.(/([\s,]|#.*$)+/).map(&:presence).compact
     @_data_providers_with_overlays_ = specs.map do |spec|
       next nil unless spec =~ /^dp:/i
       id_or_name = spec.sub(/^dp:/i,"")
