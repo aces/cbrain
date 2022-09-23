cdd3112813d55e528ee0e64051b5bb407681d5dd
Natacha Beck <natabeck@gmail.com>
2022-09-23 09:17:34 -0400

    [FIX] after PR comment

--- BrainPortal/lib/boutiques_create_fake_parent_dir.rb
+++ BrainPortal/lib/boutiques_create_fake_parent_dir.rb
@@ -21,7 +21,7 @@
 #
 
 # Some tools expect that a file/directory input is
-# placed input a parent directory.
+# placed in a parent directory.
 #
 # For example we have the following Boutiques command-line:
 #
@@ -35,17 +35,20 @@
 #       "name": "Precomputed input",
 #       "optional": false,
 #       "type": "File",
+#       "command-line-flag": "--precomputed",
 #       "value-key": "[PRECOMPUTED_INPUT]"
 #     }
 #
 # and in the `cbrain:integrator_modules` section:
 #
-#     "BoutiquesCreateFakeParentDir": ["precomputed_input"],
+#     "BoutiquesCreateFakeParentDir": {
+#       "precomputed_input": "precomputed",
+#      },
 #
 # In CBRAIN the user will select a userfile for example `sub-n`,
 # the final command line will become:
 #
-#     apptool fake_parent_dir/sub-n
+#     apptool --precomputed precomputed
 #
 module BoutiquesCreateFakeParentDir
 
@@ -59,7 +62,9 @@ module BoutiquesCreateFakeParentDir
   ############################################
 
   # Adjust the description for the input with the mention of
-  # the fake parent directory
+  # the fake parent directory information
+  # If not a the fake parent directory value is not a legal
+  # filename CBRAIN will create a valid one instead.
   def descriptor_for_form #:nodoc:
     descriptor                = super.dup
     parent_dirname_by_inputid = descriptor.custom_module_info('BoutiquesCreateFakeParentDir')
@@ -72,57 +77,60 @@ module BoutiquesCreateFakeParentDir
 
       # Adjust the description
       input              = descriptor.input_by_id(inputid)
-      input_description  = input.description || "";
-      add_to_description = "This input will be copied in a parent folder `#{fake_parent_dirname}`.The parent folder will be used in the command line"
-
-      input.description  = [input_description, add_to_description].compact.join("\n")
+      input.description  = input.description.to_s +
+                           "\nThis input will be copied in a parent folder: #{fake_parent_dirname}.The parent folder will be used in the command line"
     end
 
     descriptor
-  end
-
-  def after_form
-    after_form_msg = super
-    return after_form_msg if self.errors.any?
-
-    descriptor = descriptor_for_form
-    parent_dirname_by_inputid = descriptor.custom_module_info('BoutiquesCreateFakeParentDir')
-
-    params["BoutiquesCreateFakeParentDir"] = {}
-    parent_dirname_by_inputid.each do |inputid,fake_parent_dirname|
-      params["BoutiquesCreateFakeParentDir"][inputid] = invoke_params[inputid]
-      invoke_params[inputid] = nil
-    end
-
-    after_form_msg
   end
 
   ############################################
   # Bourreau (Cluster) Side Modifications
   ############################################
 
-  # For input in the `BoutiquesCreateFakeParentDir` section,
+  # For input in `BoutiquesCreateFakeParentDir` section,
   # create a fake parent directory that will contains a symlink
   # to the orginal selected Userfile.
   def setup #:nodoc:
-    return false unless super
 
     descriptor = self.descriptor_for_setup
     self.addlog(descriptor.file_revision_info.format("%f rev. %s %a %d"))
 
     parent_dirname_by_inputid = descriptor.custom_module_info('BoutiquesCreateFakeParentDir')
 
+    # Remove IDs from invoke_params
+    params["BoutiquesCreateFakeParentDir"] = {}
+    parent_dirname_by_inputid.each do |inputid,fake_parent_dirname|
+      params["BoutiquesCreateFakeParentDir"][inputid] = invoke_params[inputid]
+      invoke_params[inputid] = nil
+    end
+
+    # invoke main setup
+    result = super
+    return false if !super
+
+    # Re-insert IDs in invoke_params
+    invoke_params.each do |inputid, userfile_id|
+      next if userfile_id.present?
+      invoke_params[inputid] = params["BoutiquesCreateFakeParentDir"][inputid]
+    end
+
+    # Special make_available who need to have a parent folder
     parent_dirname_by_inputid.each do |inputid,fake_parent_dirname|
       userfile_id = params["BoutiquesCreateFakeParentDir"][inputid]
+
+      params['BoutiquesCreateFakeParentDir'][inputid] = nil if userfile_id.blank?
       next if userfile_id.blank?
+
       userfile    = Userfile.find(userfile_id)
 
       if !Userfile.is_legal_filename?(fake_parent_dirname)
         fake_parent_dirname = "fake_dir_#{userfile_id}"
       end
 
-      params["BoutiquesCreateFakeParentDir"][inputid] = fake_parent_dirname
       make_available(userfile, "#{fake_parent_dirname}/#{userfile.name}")
+      parent_dirname_by_inputid[inputid]              = fake_parent_dirname
+      params['BoutiquesCreateFakeParentDir'][inputid] = fake_parent_dirname
     end
 
     true
@@ -132,8 +140,13 @@ module BoutiquesCreateFakeParentDir
   # during cluster_commands()
   def finalize_bosh_invoke_struct(invoke_struct) #:nodoc:
     self.params['BoutiquesCreateFakeParentDir'].each do |inputid, dirname|
-      super[inputid] = dirname
+      if dirname.present?
+        super[inputid] = dirname
+      else
+        super.delete(inputid)
+      end
     end
-
     super
   end
+
+end
