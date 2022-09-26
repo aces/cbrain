b922080799f8c0e5b00ab368c90259cc77697b7b
Natacha Beck <natabeck@gmail.com>
2022-09-26 17:15:20 -0400

    [FIX] after PR comments

--- BrainPortal/lib/boutiques_create_fake_parent_dir.rb
+++ BrainPortal/lib/boutiques_create_fake_parent_dir.rb
@@ -25,7 +25,7 @@
 #
 # For example we have the following Boutiques command-line:
 #
-#     apptool [PRECOMPUTED_INPUT]
+#     apptool [PRECOMPUTED_INPUT] [DERIVATIVES_INPUT]
 #
 # with the following input in Boutiques:
 #
@@ -38,17 +38,27 @@
 #       "command-line-flag": "--precomputed",
 #       "value-key": "[PRECOMPUTED_INPUT]"
 #     }
+#     {
+#       "description": "A directory that contain the derivatives data process by...",
+#       "id": "derivatives_input",
+#       "name": "Derivatives input",
+#       "optional": false,
+#       "type": "File",
+#       "command-line-flag": "--derivatives",
+#       "value-key": "[DERIVATIVES_INPUT]"
+#     }
 #
 # and in the `cbrain:integrator_modules` section:
 #
 #     "BoutiquesCreateFakeParentDir": {
-#       "precomputed_input": "precomputed",
+#       "precomputed_input": ["precomputed", true],
+#       "derivatives_input": ["derivatives", false]
 #      },
 #
 # In CBRAIN the user will select a userfile for example `sub-n`,
 # the final command line will become:
 #
-#     apptool --precomputed precomputed
+#     apptool --precomputed precomputed/sub-n --derivatives derivatives
 #
 module BoutiquesCreateFakeParentDir
 
@@ -63,22 +73,16 @@ module BoutiquesCreateFakeParentDir
 
   # Adjust the description for the input with the mention of
   # the fake parent directory information
-  # If not a the fake parent directory value is not a legal
-  # filename CBRAIN will create a valid one instead.
   def descriptor_for_form #:nodoc:
     descriptor                = super.dup
     parent_dirname_by_inputid = descriptor.custom_module_info('BoutiquesCreateFakeParentDir')
 
     parent_dirname_by_inputid.each do |inputid,fake_parent_dirname|
-      fake_parent_dirname = fake_parent_dirname[0] if fake_parent_dirname.is_a?(Array)
-      if !Userfile.is_legal_filename?(fake_parent_dirname)
-        fake_parent_dirname = "fake_dir_uniq_id"
-      end
-
       # Adjust the description
       input              = descriptor.input_by_id(inputid)
+      dirname            = fake_parent_dirname[0]
       input.description  = input.description.to_s +
-                           "\nThis input will be copied in a parent folder: #{fake_parent_dirname}.The parent folder will be used in the command line"
+                           "\nThis input will be copied in a parent folder: #{dirname}.The parent folder will be used in the command line"
     end
 
     descriptor
@@ -92,61 +96,64 @@ module BoutiquesCreateFakeParentDir
   # create a fake parent directory that will contains a symlink
   # to the orginal selected Userfile.
   def setup #:nodoc:
+    original_userfile_ids = {}
+
+    basename = Revision_info.basename
+    commit   = Revision_info.short_commit
 
     descriptor = self.descriptor_for_setup
-    self.addlog(descriptor.file_revision_info.format("%f rev. %s %a %d"))
-
-    parent_dirname_by_inputid = descriptor.custom_module_info('BoutiquesCreateFakeParentDir')
 
     # Remove IDs from invoke_params
-    params["BoutiquesCreateFakeParentDir"] = {}
-    parent_dirname_by_inputid.each do |inputid,fake_parent_dirname|
-      params["BoutiquesCreateFakeParentDir"][inputid] = invoke_params[inputid]
-      invoke_params[inputid] = nil
+    parent_dirname_by_inputid = descriptor.custom_module_info('BoutiquesCreateFakeParentDir')
+    parent_dirname_by_inputid.keys do |inputid|
+      original_userfile_ids[inputid]  = invoke_params[inputid]
+      invoke_params[inputid]          = nil
     end
 
     # invoke main setup
     result = super
-    return false if !super
 
-    # Re-insert IDs in invoke_params
-    invoke_params.each do |inputid, userfile_id|
-      next if userfile_id.present?
-      invoke_params[inputid] = params["BoutiquesCreateFakeParentDir"][inputid]
-    end
+    # Return false if super failed
+    return false if ! super
 
     # Special make_available who need to have a parent folder
     parent_dirname_by_inputid.each do |inputid,fake_parent_dirname|
-      userfile_id = params["BoutiquesCreateFakeParentDir"][inputid]
+      dirname     = fake_parent_dirname[0]
+      userfile_id = original_userfile_ids[inputid]
 
-      params['BoutiquesCreateFakeParentDir'][inputid] = nil if userfile_id.blank?
       next if userfile_id.blank?
 
-      userfile    = Userfile.find(userfile_id)
+      self.addlog("Creating parent directory '#{dirname}'")
+      self.addlog("#{basename} rev. #{commit}")
 
-      if !Userfile.is_legal_filename?(fake_parent_dirname)
-        fake_parent_dirname = "fake_dir_#{userfile_id}"
-      end
-
-      make_available(userfile, "#{fake_parent_dirname}/#{userfile.name}")
-      parent_dirname_by_inputid[inputid]              = fake_parent_dirname
-      params['BoutiquesCreateFakeParentDir'][inputid] = fake_parent_dirname
+      userfile = Userfile.find(userfile_id)
+      make_available(userfile, "#{dirname}/#{userfile.name}")
     end
 
     true
+  ensure
+    original_userfile_ids.each do |inputid, userfile_id|
+      invoke_params[inputid] = original_userfile_ids[inputid]
+    end
   end
 
   # Overrides the same method in BoutiquesClusterTask, as used
   # during cluster_commands()
   def finalize_bosh_invoke_struct(invoke_struct) #:nodoc:
-    self.params['BoutiquesCreateFakeParentDir'].each do |inputid, dirname|
-      if dirname.present?
-        super[inputid] = dirname
+    invoke_params = super.dup
+
+    descriptor = self.descriptor_for_cluster_commands
+    parent_dirname_by_inputid = descriptor.custom_module_info('BoutiquesCreateFakeParentDir')
+    parent_dirname_by_inputid.each do |inputid,fake_parent_dirname|
+      if invoke_params[inputid].blank?
+        invoke_params.delete(inputid)
       else
-        super.delete(inputid)
+        (dirname, append_userfile_name) = fake_parent_dirname
+        invoke_params[inputid] = append_userfile_name ? "#{dirname}/#{invoke_params[inputid]}" : dirname
       end
     end
-    super
+
+    invoke_params
   end
 
 end
