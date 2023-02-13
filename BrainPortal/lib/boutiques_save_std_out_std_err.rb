9282ab8d1375a8df78e9914b789b4c6363ade520
Natacha Beck <natabeck@gmail.com>
2023-02-13 06:37:19 -0500

    [WIP] Add documentation

--- BrainPortal/lib/boutiques_save_std_out_std_err.rb
+++ BrainPortal/lib/boutiques_save_std_out_std_err.rb
@@ -20,7 +20,22 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# Module to save stdout and stderr files
+# This module allow to save the stdout and stderr files of a Boutiques task
+#
+# To use this module, you need to add the following lines in the descriptor:
+# "custom_module_info": {
+#   "BoutiquesSaveStdOutStdErr": {
+#     "stdout_output_dir": true,
+#     "stderr_output_dir": "path/to/dir/"
+#   }
+# }
+#
+# In case of a MultilevelSshDataProvider the "path/to/dir/" will be use to save
+# the output at this specific location.
+# In case of a no MultilevelSshDataProvider the "path/to/dir/" will be ignored.
+#
+# The value of the key "stdout_output_dir" and "stderr_output_dir" can be set to true,
+# in this situation the files will be saved in directly in the root folder of the DataProvider.
 module BoutiquesSaveStdOutStdErr
 
   # Note: to access the revision info of the module,
@@ -29,19 +44,19 @@ module BoutiquesSaveStdOutStdErr
   Revision_info=CbrainFileRevision[__FILE__] #:nodoc:
 
   # This method overrides the one in BoutiquesClusterTask
+  # Save the stdout and stderr files of the task as an output file,
+  # set it as a child of the first input file.
   def save_results
-    # Performs standard processing
     return false unless super
-    self.addlog("Saving results...")
 
     # Get the folder where to save the log files from the descriptor
     descriptor      = self.descriptor_for_save_results
     save_log_folder = descriptor.custom_module_info('BoutiquesSaveStdOutStdErr')
 
-    # Save stdout and stderr
+    # Get parent file to set stderr and stdout as children of input file
     parent_file   = Userfile.find((self.params[:invoke] ||= {})[descriptor.file_inputs.first.id])
 
-    # save stdout
+    # Save stdout
     hidden_science_stdout_basename = science_stdout_basename(self.run_number)
     science_stdout_basename        = hidden_science_stdout_basename.gsub(/^\.science./, "")
     copy_to_location               = save_log_folder["stdout_output_dir"] == true ?
@@ -50,7 +65,7 @@ module BoutiquesSaveStdOutStdErr
 
     return false if !save_log_file(hidden_science_stdout_basename, copy_to_location, parent_file)
 
-    # save stderr
+    # Save stderr
     hidden_science_stderr_basename = science_stderr_basename(self.run_number)
     science_stderr_basename        = hidden_science_stderr_basename.gsub(/^\.science./, "")
     copy_to_location               = save_log_folder["stderr_output_dir"] == true ?
@@ -62,6 +77,8 @@ module BoutiquesSaveStdOutStdErr
     true
   end
 
+  # Add the stdout and stderr files to the descriptor
+  # for the show page of the task.
   def descriptor_for_show_params  #:nodoc:
     descriptor = descriptor_for_form
 
@@ -116,7 +133,9 @@ module BoutiquesSaveStdOutStdErr
 
   private
 
-  def save_log_file(original_filename, copy_to_location, parent_file)
+  # Save the log with original_filename to copy_to_location ast
+  # a child of parent_file to the results data provider.
+  def save_log_file(original_filename, copy_to_location, parent_file) #:nodoc:
     file = safe_userfile_find_or_new(LogFile,
       :name             => copy_to_location,
       :data_provider_id => self.results_data_provider_id
@@ -128,12 +147,11 @@ module BoutiquesSaveStdOutStdErr
       self.addlog("Saved output file #{original_filename}")
       self.params["_cbrain_output_stdout"] = file.id if original_filename == science_stdout_basename(self.run_number)
       self.params["_cbrain_output_stderr"] = file.id if original_filename == science_stderr_basename(self.run_number)
+      self.save
     else
       self.addlog("Could not save back result file #{original_filename}")
       return false
     end
-
-    self.save
 
     true
   end
