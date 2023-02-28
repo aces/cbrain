566fecf09a88195de790ba21b5214a928653d53f
MontrealSergiy <sergeboroday@gmail.com>
2023-02-28 12:30:29 -0500

    prevent spurious request when setting dt filter fixes #1306

--- BrainPortal/public/javascripts/dynamic-table.js
+++ BrainPortal/public/javascripts/dynamic-table.js
@@ -167,6 +167,9 @@
 
     /* sorting, filtering and pagination requests */
     dyntbl
+      .delegate('.dt-fpop-find > input', 'keydown', function (event) {
+        if (event.keyCode == 13) event.preventDefault();
+      })
       .undelegate('.dt-sort-btn, .dt-fpop-txt, .dt-pag-pages > a', 'click.dyn-tbl')
       .delegate(  '.dt-sort-btn, .dt-fpop-txt, .dt-pag-pages > a', 'click.dyn-tbl', function (event) {
         event.preventDefault();
