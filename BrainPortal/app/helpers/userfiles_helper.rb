2b9795d79d9a4052c6725c84a8f3d606a7be7351
Natacha Beck <natabeck@gmail.com>
2023-03-31 06:01:56 -0400

    [FIX] semantic issue.

--- BrainPortal/app/helpers/userfiles_helper.rb
+++ BrainPortal/app/helpers/userfiles_helper.rb
@@ -107,7 +107,7 @@ module UserfilesHelper
           }
         ) do
           ("<span class=\"sub_viewable_link\">"+display_name+"</span>").html_safe
-        end
+      end
     elsif display_name =~ /\.html$/i # TODO: this will never happen if we ever create a HtmlFile model with at least one viewer
       link_to "#{display_name}",
         stream_userfile_path(@userfile, :file_path => file_name, :disposition => 'inline'),
