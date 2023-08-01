89d9efac96d775a29d6c0dace3f4dd4b081b7eeb
Pierre Rioux <pierre.rioux@mcgill.ca>
2023-08-01 11:24:24 -0400

    Turn off cbrain:no-run-id-for-outputs on flat DPs
    The Boutiques option 'cbrain:no-run-id-for-outputs', a custom
    option in the descriptor, normally allows the integrator to
    say that outputs created will not contain a "-1234" task ID
    suffix. This can cause a task to crush its own input (or some
    irrelevant files) at post processing. The option is now only
    implemented when the destination data provider is MultiLevel,
    presumably because in that case the output will go to a distinct
    repository by using BoutiquesForcedOutputBrowsePath.

--- BrainPortal/app/models/boutiques_cluster_task.rb
+++ BrainPortal/app/models/boutiques_cluster_task.rb
@@ -331,7 +331,8 @@ class BoutiquesClusterTask < ClusterTask
     desc   = descriptor_for_save_results
     custom = desc.custom || {} # 'custom' is not packaged as an object, just a hash
     idlist = custom['cbrain:no-run-id-for-outputs'].presence # list of IDs where no run id inserted
-    no_run_id = true if idlist && idlist.include?(output.id)
+    # We allow no_run_id only if the dest DP is MultiLevel, presumably the output goes to "a/b/c/basename_without_id"
+    no_run_id = true if idlist && idlist.include?(output.id) && self.results_data_provider.has_browse_path_capabilities?
 
     # Get basename, use it to guess the class
     name = File.basename(pathname)
