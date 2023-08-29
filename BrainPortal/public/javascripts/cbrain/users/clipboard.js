1c4f9def419edb997f131637d9e5a7ed8327ac00
MontrealSergiy <sergeboroday@gmail.com>
2023-08-29 09:25:32 -0400

    quota commit

--- BrainPortal/public/javascripts/cbrain/users/clipboard.js
+++ BrainPortal/public/javascripts/cbrain/users/clipboard.js
@@ -3,22 +3,7 @@
 #
 # CBRAIN Project
 #
-# Copyright (C) 2008-2023
-# The Royal Institution for the Advancement of Learning
-# McGill University
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# a modification of w3schools.com snippet
 #
 */
 
