176f28a42ab22075bb02fc4ab8a1851b67180e61
MontrealSergiy <sergeboroday@gmail.com>
2024-02-26 18:13:49 -0500

    Remove time_of_death from Data Provider #1373

---
+++ BrainPortal/db/migrate/20240226230749_remove_time_of_death_from_data_providers.rb
@@ -0,0 +1,27 @@
+
+#
+# CBRAIN Project
+#
+# Copyright (C) 2008-2024
+# The Royal Institution for the Advancement of Learning
+# McGill University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+class RemoveTimeOfDeathFromDataProviders < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :data_providers, :time_of_death, :datetime
+  end
+end
--- BrainPortal/db/schema.rb
+++ BrainPortal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230418205141) do
+ActiveRecord::Schema.define(version: 20240226230749) do
 
   create_table "access_profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string   "name",        null: false
@@ -104,7 +104,6 @@ ActiveRecord::Schema.define(version: 20230418205141) do
     t.text     "description",                      limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.datetime "time_of_death"
     t.boolean  "not_syncable",                                   default: false, null: false
     t.string   "time_zone"
     t.string   "cloud_storage_client_identifier"
@@ -122,7 +121,7 @@ ActiveRecord::Schema.define(version: 20230418205141) do
     t.index ["user_id"], name: "index_data_providers_on_user_id", using: :btree
   end
 
-  create_table "data_usage", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "data_usage", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci" do |t|
     t.integer  "user_id",                          null: false
     t.integer  "group_id",                         null: false
     t.string   "yearmonth",                        null: false
@@ -138,7 +137,7 @@ ActiveRecord::Schema.define(version: 20230418205141) do
     t.datetime "updated_at",                       null: false
   end
 
-  create_table "disk_quotas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "disk_quotas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci" do |t|
     t.integer  "user_id"
     t.integer  "data_provider_id"
     t.decimal  "max_bytes",        precision: 24
@@ -184,7 +183,7 @@ ActiveRecord::Schema.define(version: 20230418205141) do
     t.index ["type"], name: "index_groups_on_type", using: :btree
   end
 
-  create_table "groups_editors", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "groups_editors", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci" do |t|
     t.integer "group_id"
     t.integer "user_id"
     t.index ["group_id", "user_id"], name: "index_groups_editors_on_group_id_and_user_id", unique: true, using: :btree
@@ -205,7 +204,7 @@ ActiveRecord::Schema.define(version: 20230418205141) do
     t.index ["key"], name: "index_help_documents_on_key", unique: true, using: :btree
   end
 
-  create_table "large_session_infos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "large_session_infos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci" do |t|
     t.string   "session_id",                               null: false
     t.text     "data",       limit: 65535
     t.datetime "created_at"
@@ -296,7 +295,7 @@ ActiveRecord::Schema.define(version: 20230418205141) do
     t.index ["type"], name: "index_remote_resources_on_type", using: :btree
   end
 
-  create_table "resource_usage", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "resource_usage", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci" do |t|
     t.string   "type"
     t.decimal  "value",                    precision: 24
     t.integer  "user_id"
@@ -384,6 +383,7 @@ ActiveRecord::Schema.define(version: 20230418205141) do
     t.boolean  "hidden",                           default: false
     t.integer  "remote_resource_id"
     t.string   "form_page"
+    t.string   "maillist_consent"
   end
 
   create_table "sites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
@@ -465,6 +465,7 @@ ActiveRecord::Schema.define(version: 20230418205141) do
     t.string   "container_exec_args"
     t.boolean  "inputs_readonly",                             default: false
     t.string   "boutiques_descriptor_path"
+    t.boolean  "copy_input",                                  default: false
     t.index ["bourreau_id"], name: "index_tool_configs_on_bourreau_id", using: :btree
     t.index ["tool_id"], name: "index_tool_configs_on_tool_id", using: :btree
   end
@@ -540,6 +541,8 @@ ActiveRecord::Schema.define(version: 20230418205141) do
     t.boolean  "account_locked",       default: false, null: false
     t.string   "zenodo_main_token"
     t.string   "zenodo_sandbox_token"
+    t.string   "maillist_consent"
+    t.string   "envoke_id"
     t.index ["login"], name: "index_users_on_login", using: :btree
     t.index ["type"], name: "index_users_on_type", using: :btree
   end
