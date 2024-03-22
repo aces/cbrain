1841b716bc8b862c47d9a3aa4b497c198d212323
MontrealSergiy <sergeboroday@gmail.com>
2024-03-22 17:05:00 -0400

    rename singularity to apptainer in variables (merge migrations)

--- BrainPortal/db/migrate/20231214214407_rename_singularity_to_apptainer_overlays_specs.rb
+++
@@ -1,5 +0,0 @@
-class RenameSingularityToApptainerOverlaysSpecs < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :tool_configs, :singularity_overlays_specs, :apptainer_overlays_specs
-  end
-end
--- BrainPortal/db/migrate/20231214214549_rename_singularity_to_apptainer_executable_name.rb
+++
@@ -1,5 +0,0 @@
-class RenameSingularityToApptainerExecutableName < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :remote_resources, :singularity_executable_name, :apptainer_executable_name
-  end
-end
