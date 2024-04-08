0c5c3598d5b6b5782258fd39231ddb63b77717e5
MontrealSergiy <sergeboroday@gmail.com>
2024-04-08 17:14:21 -0400

    Apptainer rename - timestamp update in schema.rb

--- BrainPortal/db/schema.rb
+++ BrainPortal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20231214214549) do
+ActiveRecord::Schema.define(version: 20240408164549) do
 
   create_table "access_profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string   "name",        null: false
