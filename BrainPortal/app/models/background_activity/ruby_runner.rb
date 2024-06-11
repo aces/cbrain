f0716f7a951645aed85f19e558de2c2a7ef54e9d
Pierre Rioux <pierre.rioux@mcgill.ca>
2024-06-11 11:14:59 -0400

    Adjusted RubyRunner
    The names of the options for the code pieces
    match the names of the methods.

--- BrainPortal/app/models/background_activity/ruby_runner.rb
+++ BrainPortal/app/models/background_activity/ruby_runner.rb
@@ -24,44 +24,43 @@
 #
 # The options has should contain the following entries:
 #
-#   :prepare_items     => 'ruby code to prepare items'
-#   :before_first_item => 'ruby code to run before the first item is processed'
-#   :after_last_item   => 'ruby code to run after the last item is processed'
-#   :process_item      => 'ruby code to process one item'
+#   :prepare_dynamic_items => 'ruby code to prepare items'
+#   :before_first_item     => 'ruby code to run before the first item is processed'
+#   :after_last_item       => 'ruby code to run after the last item is processed'
+#   :process               => 'ruby code to process one item'
 #
-# Although these keys look similar to the names to some of the methods of the
-# framework, the values are not the methods, but just pure ruby text invoked
-# by the respective the methods.
+# Although these keys have the same names as the core methods of the
+# framework, the values are not the methods, but just pure Ruby text code.
 #
 # The code will be executed by, respectively, the actual methods
 # prepare_dynamic_items(), before_first_item(), after_last_item() and
 # process(item).
 #
 # In all case, the code's binding will be the current BackgroundActivity
-# object, and in the case of process_item(), the item itself will be in
+# object, and in the case of process(), the +item+ itself will be within
 # the closure.
 #
 # Example of an options hash: this create a BAC that computes the square of the numbers
 # listed in the items list, but only if the item is even.
 #
 #    bac.options = {
-#      :prepare_items => 'self.items = [1,2,3,4]',
-#      :process_item  =>  'return [ true, item*item ] if item % 2 == 0;return [ false, "Is Odd" ]'
+#      :prepare_dynamic_items => 'self.items = [1,2,3,4]',
+#      :process               => 'return [true,item*item] if item % 2 == 0;return [false,"Odd"]'
 #    }
 class BackgroundActivity::RubyRunner < BackgroundActivity
 
   Revision_info=CbrainFileRevision[__FILE__] #:nodoc:
 
-  validates_dynamic_bac_presence_of_option :process_item
+  validates_dynamic_bac_presence_of_option :process
 
   def prepare_dynamic_items
-    code = options[:prepare_items].to_s.presence
+    code = options[:prepare_dynamic_items].to_s.presence
     return if code.blank?
     eval code
   end
 
   def process(item)
-    code = options[:process_item].to_s.presence
+    code = options[:process].to_s.presence
     return self.internal_error! if code.blank?
     eval code
   end
