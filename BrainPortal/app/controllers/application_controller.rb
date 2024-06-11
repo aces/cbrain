765d1ff408b4c03649d196148209d1747dc1e5b4
Natacha Beck <nbeck.nan@gmail.com>
2024-06-11 15:50:10 +0000

    [WIP]

--- BrainPortal/app/controllers/application_controller.rb
+++ BrainPortal/app/controllers/application_controller.rb
@@ -135,7 +135,7 @@ class ApplicationController < ActionController::Base
   # buttons and explanations.
   def check_mandatory_globus_id_linkage #:nodoc:
     return true if ! user_must_link_to_globus?(current_user)
-    return true if user_has_link_to_globus?(current_user)
+    return true if   user_has_link_to_globus?(current_user)
     respond_to do |format|
       format.html { redirect_to :controller => :sessions, :action => :mandatory_globus }
       format.json { render :status => 403, :json => { "error" => "This account must first be linked to a Globus identity" } }
