03048a5e12636fa42b1d89edbc2831a361714d20
Natacha Beck <777590+natacha-beck@users.noreply.github.com>
2024-06-12 06:23:13 -0400

    Update nh_sessions_controller.rb

--- BrainPortal/app/controllers/nh_sessions_controller.rb
+++ BrainPortal/app/controllers/nh_sessions_controller.rb
@@ -157,7 +157,7 @@ class NhSessionsController < NeurohubApplicationController
     token_uri = oidc_config[:token_uri]
 
     # Query Globus; this returns all the info we need at the same time.
-    identity_struct = globus_fetch_token(code, nh_globus_url, token_uri, oidc_name) # globus_url is generated from routes
+    identity_struct = globus_fetch_token(code, nh_globus_url, token_uri, oidc_name) # nh_globus_url is generated from routes
     if !identity_struct
       cb_error "Could not fetch your identity information from #{oidc_name}"
     end
