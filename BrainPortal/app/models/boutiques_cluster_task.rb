c253733a89d9fa8ee45d989d61d7035e3858dc2b
MontrealSergiy <sergeboroday@gmail.com>
2024-07-02 16:14:54 -0400

    optimize local bind mount - fixes/factor out extra method to enumerate inputs dataproviders

--- BrainPortal/app/models/boutiques_cluster_task.rb
+++ BrainPortal/app/models/boutiques_cluster_task.rb
@@ -100,12 +100,15 @@ class BoutiquesClusterTask < ClusterTask
 
   # narrows down local dp paths only to the most relevant, to be used at setup
   def local_dp_storage_paths
+    return super & input_file_dps.pluck(:remote_dir)
+  end
+
+  # lists the dp involved into input files
+  def input_file_dps
     file_ids = descriptor_for_setup.file_inputs.map do |input|
       invoke_params[input.id]
     end
-
-    relevant_dp_ids = Userfile.where(id: file_ids).compact.uniq
-    return super & DataProvider.where(id: relevant_dp_ids).pluck(:remote_dir)
+    return Userfile.where(id: file_ids).map &:data_provider
   end
 
   def cluster_commands #:nodoc:
