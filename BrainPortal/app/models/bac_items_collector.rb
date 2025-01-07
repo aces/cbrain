adff85412f1432973d546380444ab9666e57be37
Pierre Rioux <pierre.rioux@mcgill.ca>
2025-01-07 12:33:24 -0500

    Adjusted BacItemsCollector: new methods, new doc

--- BrainPortal/app/models/bac_items_collector.rb
+++ BrainPortal/app/models/bac_items_collector.rb
@@ -23,12 +23,12 @@
 # This class provides a utility 'aggregator' or 'collector' for the
 # list of items of BackgroundActivity (aka 'BAC') objects.
 #
-# The collector object will be created and configured with
+# The collector object is created and configured with
 # a max number of items, and then the +add_items+ method
-# will be invoked repetitively to add one or many items to a
+# can be invoked repetitively to add one or many items to a
 # list maintained internally by the collector. Whenever that list
 # exceeds in length the max number of items, a BAC object is created
-# and save to the database with that number of items.
+# and saved to the database with that number of items.
 #
 # The collector is created by providing an existing BAC object (which doesn't
 # even need to be saved to the database) as the model of all the BAC
@@ -58,6 +58,10 @@
 #   # Create a MoveFile BAC for the remaining two items.
 #   col.flush
 #
+#   # Get a list of the IDs of all the BACs the collector
+#   # has created so far.
+#   bac_ids = col.submitted_bac_ids # array of IDs
+#
 class BacItemsCollector
 
   attr_reader :submitted_bac_ids, :max_items
@@ -70,25 +74,23 @@ class BacItemsCollector
   # the database (a new() object works just as well). All its attributes
   # must be properly initialized such that it COULD be saved, except
   # for the items list and the counters. The collector keeps its own
-  # copy of the object provided in new, and resets the items and counters
-  # internally.
+  # copy of the object, and resets the items and counters internally.
   #
   # The +max_items+ argument controls how many items needs to be provided
-  # (using +add_items+) for the collector to create a true BAC object.
+  # (using +add_items+) for the collector to create a BAC object.
   def initialize(bac_template,max_items)
 
     # We get a BackgroundActivity object that we will use as a template.
     # We initialize it to 'no items' and with counters at 0, but leave the rest alone.
-    @bac       = bac_template.dup
-    @bac.items = []
-    @bac.reset!(@bac.status)
+    @bac            = bac_template.dup
+    @bac.items      = []
     @bac.created_at = nil
     @bac.updated_at = nil
+    @bac.reset!(@bac.status) # zap messages and counters
 
     # Some simple validations
     raise "The BackgroundActivity has no user_id."            unless @bac.user_id
     raise "The BackgroundActivity has no remote_resource_id." unless @bac.remote_resource_id
-    raise "The BackgroundActivity has no status."             unless @bac.status
     raise "The BackgroundActivity has an invalid status."     unless
       [ 'InProgress', 'Scheduled', 'Suspended' ].include?(@bac.status)
 
@@ -103,8 +105,10 @@ class BacItemsCollector
   end
 
   # Add one or several items; items can be a single items or an array of items.
-  # If the added items makes the list of items exceed in length the parameter
-  # max_items, then we submit the internal BAC object and initialize a new one.
+  # If the added items makes the collector's internal list of items exceed in length the parameter
+  # max_items, then the collector will save its internal BAC object to the DB,
+  # and prepare a new internal one for future items.
+  #
   # The method will safely accept large arrays that trigger several such saves.
   #
   # Returns the number of items currently in the collector's memory.
@@ -125,6 +129,32 @@ class BacItemsCollector
     @bac.items += new_items
 
     @bac.items.size
+  end
+
+  # This method is like +add_items+ but the +item+ provided
+  # must be a single item; if an array is provided, the
+  # array itself will be added as a single item.
+  #
+  #  col << 34    # push the single item 34
+  #  col << [1,2] # push the array [1,2] as a single item
+  def <<(item)
+    add_items( [ item ] )
+  end
+
+  # This method is like +add_items+ but the +items+ provided
+  # must be an Enumerable. An exception will be raised
+  # otherwise. Unlike +add_items+ this method returns self
+  # (the BacItemsCollector object itelf). The recommended
+  # way of using the "+" operator is through the "+=" operator
+  # instead, even though this would also work with a bare "+".
+  #
+  #   col += [ 1, 2, 3]  # recommended
+  #   col +  [ 1, 2, 3]  # NOT recommended; still updates col
+  #
+  def +(items)
+    raise TypeError.new("BacItemsCollector requires an Enumerable for +=") unless items.is_a?(Enumerable)
+    add_items(items)
+    self
   end
 
   # Invoke this method to 'flush' the remaining items,
