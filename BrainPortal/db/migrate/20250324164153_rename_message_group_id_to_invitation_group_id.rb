8b4ddf003cd7d17dc09fa8746f8b6a6a0a85ab68
axif <muhamadasif570@gmail.com>
2025-03-24 23:22:21 +0600

    revert migration checks

--- BrainPortal/db/migrate/20250324164153_rename_message_group_id_to_invitation_group_id.rb
+++ BrainPortal/db/migrate/20250324164153_rename_message_group_id_to_invitation_group_id.rb
@@ -1,27 +1,9 @@
 class RenameMessageGroupIdToInvitationGroupId < ActiveRecord::Migration[5.0]
   def up
-    # Add invitation_group_id if neither column exists.
-    unless column_exists?(:messages, :group_id) || column_exists?(:messages, :invitation_group_id)
-      add_column :messages, :invitation_group_id, :integer
-      return
-    end
-    
-    # Rename if old column exists and new doesn't.
-    if column_exists?(:messages, :group_id) && !column_exists?(:messages, :invitation_group_id)
-      rename_column :messages, :group_id, :invitation_group_id
-    end
+    rename_column :messages, :group_id, :invitation_group_id
   end
 
   def down
-    # Add group_id if neither column exists.
-    unless column_exists?(:messages, :group_id) || column_exists?(:messages, :invitation_group_id)
-      add_column :messages, :group_id, :integer
-      return
-    end
-    
-    # Rename if new column exists and old doesn't.
-    if column_exists?(:messages, :invitation_group_id) && !column_exists?(:messages, :group_id)
-      rename_column :messages, :invitation_group_id, :group_id
-    end
+    rename_column :messages, :invitation_group_id, :group_id
   end
 end 
