1cf1dd9519f3bab2a37e1ce432bb7a569125c83d
MontrealSergiy <sergeboroday@gmail.com>
2025-03-24 19:06:18 -0400

    Refactor singularity into apptainer - update migration timestamp

--- BrainPortal/db/schema.rb
+++ BrainPortal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20250213205048) do
+ActiveRecord::Schema.define(version: 20250324164549) do
 
   create_table "access_profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci" do |t|
     t.string   "name",        null: false
