1d9d32e57e8da299c194eba4b0e034fa42214a60
MontrealSergiy <sergeboroday@gmail.com>
2025-04-01 15:56:19 -0400

    drop html streaming

--- BrainPortal/app/helpers/userfiles_helper.rb
+++ BrainPortal/app/helpers/userfiles_helper.rb
@@ -108,10 +108,6 @@ module UserfilesHelper
         ) do
           ("<span class=\"sub_viewable_link\">"+display_name+"</span>").html_safe
       end
-    elsif display_name =~ /\.html$/i # TODO: this will never happen if we ever create a HtmlFile model with at least one viewer
-      link_to "#{display_name}",
-        stream_userfile_path(@userfile, :file_path => file_name, :disposition => 'inline'),
-        :target => '_BLANK'
     else
       link_to h(display_name),
               url_for(:action  => :content, :content_loader => :collection_file, :arguments => file_name)
