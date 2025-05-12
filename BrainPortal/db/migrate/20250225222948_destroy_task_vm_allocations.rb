2a291ab7e9964848a72b0e945e6b460ca0c55513
MontrealSergiy <sergeboroday@gmail.com>
2025-05-12 19:05:29 -0400

    remove redundant migration #1375

--- BrainPortal/db/migrate/20250225222948_destroy_task_vm_allocations.rb
+++
@@ -1,5 +0,0 @@
-class DropTaskVmAllocations < ActiveRecord::Migration[5.0]
-  def up
-    TaskVmAllocation.destroy_all if defined?(TaskVmAllocation)
-  end
-end
--- BrainPortal/db/migrate/20250225222958_drop_task_vm_allocations.rb
+++
@@ -1,5 +0,0 @@
-class DropTaskVmAllocations < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :task_vm_allocations
-  end
-end
