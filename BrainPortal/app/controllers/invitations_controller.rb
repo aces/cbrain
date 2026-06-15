1f36000e27c742a978b91899df03fbedc06e0b0a
Pierre Rioux <pierre.rioux@mcgill.ca>
2026-06-15 12:00:06 -0400

    Adjusted group invitation form.
    Now we allow users to enter usernames or emails.

--- BrainPortal/app/controllers/invitations_controller.rb
+++ BrainPortal/app/controllers/invitations_controller.rb
@@ -48,36 +48,68 @@ class InvitationsController < ApplicationController
   # Send an invitation
   def create #:nodoc:
     @group          = Group.find(params[:group_id])
-    user_ids        = (params[:user_ids] || []).map(&:to_i)
-    already_sent_to = Invitation.where(sender_id: current_user.id, active: true, user_id: user_ids, invitation_group_id: @group.id).all.map(&:user_id)
-    rejected_ids    = user_ids & already_sent_to
 
-    if user_ids.empty?
-      flash_message = "\nYou should select at the least one user."
-    elsif rejected_ids.present?
-      flash_message = "\n#{User.find(rejected_ids).map(&:login).join(", ")} already invited."
-    end
-
-    @users = User.find((user_ids - already_sent_to) & current_user.visible_users.map(&:id))
-
-    unless @group.can_be_edited_by?(current_user) && @users.present?
-      flash[:error]  = "Could not send the requested invitations."
-      flash[:error] += flash_message if flash_message.present?
-      respond_to do |format|
+    unless @group.can_be_edited_by?(current_user)
+       flash[:error] = "You don't have permission send invitations for this project."
+       respond_to do |format|
         format.html { redirect_to group_path(@group) }
         format.xml  { head :forbidden }
-      end
-      return
+       end
+       return
     end
 
-    Invitation.send_out(current_user, @group, @users)
-    flash[:notice] = "Your invitations were successfully sent."
-    flash[:notice] += flash_message if rejected_ids.present?
+    # The form allows users to invite by emails or usernames, even though
+    # the parameter is only called :emails
+    user_specs      = (params[:emails].presence.try(:strip) || "").split(/[\s,]+/)
+    user_specs      = user_specs.map(&:presence).compact
 
-    respond_to do |format|
-     format.html { redirect_to group_path(@group) }
-     format.xml  { head :ok }
+    if user_specs.empty?
+      cb_error "Please specify at least one email address or username", :redirect => group_path(@group)
     end
+
+    # Fetch the users
+    uids_by_email   = User.where(:email => user_specs).pluck(:id)
+    uids_by_uname   = User.where(:login => user_specs).pluck(:id)
+    user_ids        = uids_by_email | uids_by_uname
+    found_users     = User.where(:id => user_ids).to_a
+    found_specs     = user_specs.select do |spec|
+      found_users.any? { |u| u.login == spec || u.email == spec }
+    end
+
+    # Ok, which ones are not found?
+    not_found_specs = user_specs - found_specs
+
+    flash_notice = []
+    flash_errors = []
+    if not_found_specs.present?
+      flash_errors.push <<-MESSAGE
+        We are not able to invite user(s) identified by: #{not_found_specs.join(", ")}.
+        At the moment users are matched by emails or usernames.
+        Please confirm with your collaborators which email or username they use in CBRAIN.
+      MESSAGE
+    end
+
+    # Which invitations are pending?
+    already_sent_to = Invitation.where(active: true, user_id: user_ids, invitation_group_id: @group.id).pluck(:user_id)
+    rejected_ids    = user_ids & already_sent_to
+    if rejected_ids.present?
+      already_logins = User.where(:id => rejected_ids).pluck(:login).join(", ")
+      flash_errors.push "Already invited: #{already_logins}"
+    end
+
+    # List of newly invited users
+    invited_users = User.find(user_ids - already_sent_to - @group.user_ids)
+    if invited_users.present?
+      Invitation.send_out(current_user, @group, invited_users)
+      flash_notice.push "Your invitation was successfully sent to #{view_pluralize(invited_users.size,"user")}"
+    else
+      flash_errors.push "No new users were found to invite."
+    end
+
+    flash[:notice]  = flash_notice.join "\n" if flash_notice.present?
+    flash[:error]   = flash_errors.join "\n" if flash_errors.present?
+
+    redirect_to group_path(@group)
   end
 
   # Accept an invitation
