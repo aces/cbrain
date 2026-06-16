f17f846260a9cb4f9cc2a7ff3c769c85a3312d54
natacha-beck <natacha.beck@mcgill.ca>
2026-06-16 13:16:57 -0400

    [FIX] in order to display tag that is in Everyone group

--- BrainPortal/app/models/userfile.rb
+++ BrainPortal/app/models/userfile.rb
@@ -287,7 +287,7 @@ class Userfile < ApplicationRecord
   # by +user+. Actually returns a ActiveRecord::Relation.
   def get_tags_for_user(user)
     user = User.find(user) unless user.is_a?(User)
-    self.tags.where('tags.user_id' => user.id)
+    self.tags.where('tags.user_id' => user.id).or(self.tags.where('tags.group_id' => Group.everyone.id))
   end
 
   # Set the tags associated with this file to those
